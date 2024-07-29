d09449808e836864300fde570542a834892fd5e5
Galur Arasy <adamhenderson3x3@gmail.com>
2024-07-29 15:27:33 +0700

    update: props safe typing + add user information in navbar

--- resources/js/Layouts/Partials/Navbar.tsx
+++ resources/js/Layouts/Partials/Navbar.tsx
@@ -1,9 +1,25 @@
-import { RiNotification4Line, RiSearchLine } from "@remixicon/react";
+﻿import { RiNotification4Line, RiSearchLine } from "@remixicon/react";
 import { Input } from "@/Components/ui/input";
 import { Separator } from "@/Components/ui/separator";
 import { Avatar, AvatarFallback, AvatarImage } from "@/Components/ui/avatar";
+import { usePage } from "@inertiajs/react";
+import { useEffect } from "react";
 
 export default function Navbar() {
+    const { auth } = usePage().props;
+
+    useEffect(() => {
+        document.addEventListener("keydown", (e) => {
+            if (e.ctrlKey && e.key === "k") {
+                e.preventDefault();
+                document.getElementById("search")?.focus();
+            }
+        });
+
+        return () => {
+            document.removeEventListener("keydown", () => {});
+        };
+    }, []);
     return (
         <nav className="flex  h-16 border-b-2 justify-between items-center px-4 py-3">
             <div className="w-64 h-full flex items-center rounded border-2 px-2">
@@ -27,11 +43,11 @@ export default function Navbar() {
                 <Separator orientation="vertical" className="w-[2px]" />
                 <Avatar>
                     <AvatarImage />
-                    <AvatarFallback>JD</AvatarFallback>
+                    <AvatarFallback>{auth?.user.initials}</AvatarFallback>
                 </Avatar>
                 <div className="flex flex-col">
                     <span className="text-navbar-primary-foreground text-sm">
-                        John Doe
+                        {auth?.user.name}
                     </span>
                     <span className="text-navbar-secondary-foreground text-sm">
                         Supervisor
