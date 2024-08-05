ff659bc6f03ea7af1e9d6f59a27bbeae12846be0
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-08-05 14:11:10 +0700

    add animation AnimateIn

--- resources/js/Pages/CreateProject/CreateProject.tsx
+++ resources/js/Pages/CreateProject/CreateProject.tsx
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, useForm } from '@inertiajs/react';
 import { FormEventHandler } from 'react';
 import { useToast } from '@/Components/ui/use-toast';
+import AnimateIn from '@/lib/AnimateIn';
 
 type Project = {
     nomorProyek: number;
@@ -29,14 +30,20 @@ export default function CreateProject() {
             <Head title="Buat Proyek" />
             <div className="py-12">
                 <div className="max-w-7xl h-screen sm:px-6 lg:px-8 space-y-6 ">
-                    <form onSubmit={submit}>
-                        <div className="p-4 sm:p-8 bg-white h-[20rem] dark:bg-gray-800 shadow sm:rounded-lg">
-                            <h1 className="text-2xl font-bold">Buat Proyek</h1>
-                            <h3 className="text-lg mt-2">Nomor Projek :</h3>
+                    <AnimateIn
+                        from="opacity-0 -translate-y-4"
+                        to="opacity-100 translate-y-0 translate-x-0"
+                        duration={300}
+                    >
+                        Animate In!
+                        <form onSubmit={submit}>
+                            <div className="p-4 sm:p-8 bg-white h-[20rem] dark:bg-gray-800 shadow sm:rounded-lg">
+                                <h1 className="text-2xl font-bold">Buat Proyek</h1>
+                                <h3 className="text-lg mt-2">Nomor Projek :</h3>
 
-                            {/* <InputLabel htmlFor="name" value="Name" /> */}
+                                {/* <InputLabel htmlFor="name" value="Name" /> */}
 
-                            {/* <TextInput
+                                {/* <TextInput
                         id="name"
                         className="mt-1 block w-full"
                         value={data.name}
@@ -45,33 +52,34 @@ export default function CreateProject() {
                         isFocused
                         autoComplete="name"
                     /> */}
-                            <div className="w-64 flex items-center rounded border-2 px-2 mt-3 ">
-                                <Input
-                                    value={data.nomorProyek}
-                                    onChange={e => setData('nomorProyek', Number(e.target.value))}
-                                    className="h-full border-none focus-visible:ring-0 focus-visible:ring-offset-0"
-                                    placeholder="Buat Proyek"
-                                    type="number"
-                                />
-                            </div>
-                            <h3 className="text-lg mt-2">Jumlah Trainset :</h3>
+                                <div className="w-64 flex items-center rounded border-2 px-2 mt-3 ">
+                                    <Input
+                                        value={data.nomorProyek}
+                                        onChange={e => setData('nomorProyek', Number(e.target.value))}
+                                        className="h-full border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                                        placeholder="Buat Proyek"
+                                        type="number"
+                                    />
+                                </div>
+                                <h3 className="text-lg mt-2">Jumlah Trainset :</h3>
 
-                            <div className="w-64 flex items-center rounded border-2 px-2 mt-3 ">
-                                <Input
-                                    value={data.jumlahTrainset}
-                                    onChange={e => setData('jumlahTrainset', Number(e.target.value))}
-                                    className="h-full border-none focus-visible:ring-0 focus-visible:ring-offset-0"
-                                    placeholder="Jumlah Trainset"
-                                    type="number"
-                                />
+                                <div className="w-64 flex items-center rounded border-2 px-2 mt-3 ">
+                                    <Input
+                                        value={data.jumlahTrainset}
+                                        onChange={e => setData('jumlahTrainset', Number(e.target.value))}
+                                        className="h-full border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                                        placeholder="Jumlah Trainset"
+                                        type="number"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex justify-end">
-                            <button type="submit" className="p-2 mt-5 bg-blue-600 rounded-md text-white">
-                                Buat Proyek
-                            </button>
-                        </div>
-                    </form>
+                            <div className="flex justify-end">
+                                <button type="submit" className="p-2 mt-5 bg-blue-600 rounded-md text-white">
+                                    Buat Proyek
+                                </button>
+                            </div>
+                        </form>
+                    </AnimateIn>
                 </div>
             </div>
         </AuthenticatedLayout>
