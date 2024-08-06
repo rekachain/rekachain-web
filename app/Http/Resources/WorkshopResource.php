6cf85b7909f2ceaae563abc6979fef9c4c1fac5e
ChamzalIsal <hhamzalnur10@gmail.com>
2024-08-06 14:27:24 +0700

    Update: Typo Model Name Workshop
    + Workshop Resource
    + Model Workshop

--- app/Http/Resources/WorkshopResource.php
+++ app/Http/Resources/WorkshopResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class DivisionResource extends JsonResource {
+class WorkshopResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
