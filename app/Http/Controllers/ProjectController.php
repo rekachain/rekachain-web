37127567c64e11955bc9db49c6a72e01d381d973
N00budrag0n <helmiagung2468@gmail.com>
2024-08-06 08:41:36 +0700

    Project done, kurang relasi ke tabel attachment

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,16 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Project\StoreProjectRequest;
+use App\Http\Requests\Project\UpdateProjectRequest;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
+use App\Support\Interfaces\ProjectServiceInterface;
 use Illuminate\Http\Request;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 
 class ProjectController extends Controller
 {
+    public function __construct(protected ProjectServiceInterface $projectService) {}
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        if ($this->ajax()) {
+            try {
+                $perPage = request()->get('perPage', 5);
+                return ProjectResource::collection($this->projectService->getAllPaginated($request->query(), $perPage));
+            } catch (NotFoundExceptionInterface|ContainerExceptionInterface $e) {
+            }
+        }
+        return inertia('Project/Index');
     }
 
     /**
@@ -19,7 +34,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return intertia('Project/Create');
     }
 
     /**
@@ -27,38 +42,46 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        if ($this->ajax()) {
+            return $this->projectService->create($request->validated());
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Project $project)
     {
-        //
+        if ($this->ajax()) {
+            return new ProjectResource($project);
+        }
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Project $project)
     {
-        //
+        return intertia('Project/Edit', ['project' => new ProjectResource($project)]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateProjectRequest $request, Project $project)
     {
-        //
+        if ($this->ajax()) {
+            return $this->projectService->update($request->validated());
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, Project $project)
     {
-        //
+        if ($this->ajax()) {
+            return $this->projectService->delete($project);
+        }
     }
 }
