dead32a27d2a523bf6b23ae9398b95257152ee54
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-08-14 09:15:30 +0700

    add add panel and gerbong

--- resources/js/Pages/CreateProject/DetailTrainset.tsx
+++ resources/js/Pages/CreateProject/DetailTrainset.tsx
@@ -7,37 +7,45 @@ import { Button } from '@/Components/ui/button';
 import { useState } from 'react';
 
 type Gerbong = {
+    id: number;
     namaGerbong: string;
     panel: Panel[];
 };
 
 type Panel = {
+    id: number;
     namaPanel: string;
     jumlahPanel: number;
 };
 
 const listGerbongConst: Gerbong[] = [
     {
+        id: 0,
         namaGerbong: 'K1',
         panel: [
             {
+                id: 0,
                 namaPanel: 'AC',
                 jumlahPanel: 20,
             },
             {
+                id: 1,
                 namaPanel: 'Mainboard',
                 jumlahPanel: 11,
             },
         ],
     },
     {
+        id: 1,
         namaGerbong: 'M',
         panel: [
             {
+                id: 1,
                 namaPanel: 'AC',
                 jumlahPanel: 20,
             },
             {
+                id: 2,
                 namaPanel: 'Mainboard',
                 jumlahPanel: 11,
             },
@@ -46,7 +54,58 @@ const listGerbongConst: Gerbong[] = [
 ];
 
 export default function DetailTrainset() {
-    const [listGerbong, setListGerbong] = useState<Gerbong[]>(listGerbongConst);
+    const [listGerbong, setListGerbong] = useState<Gerbong[] | undefined>(listGerbongConst);
+
+    function tambahGerbong() {
+        setListGerbong([
+            ...listGerbong!,
+            { id: listGerbong!.length + 1, namaGerbong: 'K3', panel: [{ id: 1, namaPanel: 'PIDS', jumlahPanel: 13 }] },
+        ]);
+    }
+    function tambahPanel(id: number) {
+        // setListGerbong(
+        //     listGerbong!.map(gerbong => {
+        //         if (gerbong.id == id) {
+        //             return listGerbong;
+        //         } else {
+        //         }
+        //     }),
+        //    );
+        // setListGerbong(
+        //     listGerbong!.map(gerbong => {
+        //         if (gerbong.id === id) {
+        //             const gerbongPanel = gerbong.panel.map(pan => {
+        //                 if (pan.id === idPanel) {
+        //                     return [
+        //                         ...gerbong.panel,
+        //                         { id: gerbong.panel.length + 1, namaPanel: 'Mainboard', jumlahPanel: 18 },
+        //                     ];
+        //                 }
+        //                 return pan
+        //             });
+        //             // return {...joke, likes: joke.likes + 1}
+        //         } else {
+        //             gerbong;
+        //         }
+        //     }),
+        // );
+        setListGerbong(
+            listGerbong!.map(gerbong => {
+                if (gerbong.id === id) {
+                    return {
+                        ...gerbong,
+                        panel: [
+                            ...gerbong.panel,
+                            { id: gerbong.panel.length + 1, namaPanel: 'Mainboard', jumlahPanel: 18 },
+                        ],
+                    };
+                    // }
+                }
+                return gerbong;
+            }),
+        );
+    }
+
     return (
         <div className="">
             <AuthenticatedLayout>
@@ -60,7 +119,7 @@ export default function DetailTrainset() {
                             duration={300}
                         >
                             {/* <div className="p-4 sm:p-8 bg-white h-fit dark:bg-gray-800 shadow sm:rounded-lg"> */}
-                            {listGerbong.map(gerbong => (
+                            {listGerbong!.map(gerbong => (
                                 <Accordion type="single" collapsible>
                                     <AccordionItem value="item-1">
                                         <AccordionTrigger>{gerbong.namaGerbong}</AccordionTrigger>
@@ -70,7 +129,9 @@ export default function DetailTrainset() {
                                                 <div className="flex mt-2 gap-4">
                                                     <Input placeholder="Nama Panel"></Input>
                                                     <Input placeholder="Jumlah"></Input>
-                                                    <Button>Tambah Panel</Button>
+                                                    <Button onClick={() => tambahPanel(gerbong.id)}>
+                                                        Tambah Panel
+                                                    </Button>
                                                 </div>
                                                 <hr className="mt-4 border-black" />
                                                 <div className="">
@@ -95,7 +156,9 @@ export default function DetailTrainset() {
                                     </AccordionItem>
                                 </Accordion>
                             ))}
-                            <Button className="mt-5 w-full">Tambah Gerbong</Button>
+                            <Button className="mt-5 w-full" onClick={tambahGerbong}>
+                                Tambah Gerbong
+                            </Button>
                             {/* </div> */}
                         </AnimateIn>
                     </div>
