09dc1a930397903c7e269c33ca1e388768d23a37
Galur Arasy <adamhenderson3x3@gmail.com>
2024-08-16 08:07:14 +0700

    update: persistent sidebar collapsible menu

--- resources/js/Layouts/Partials/Components/SidebarLinkCollapsible.tsx
+++ resources/js/Layouts/Partials/Components/SidebarLinkCollapsible.tsx
@@ -1,17 +1,35 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/Components/ui/accordion';
 import { buttonVariants } from '@/Components/ui/button';
 import { Link } from '@inertiajs/react';
-import React from 'react';
+import React, { useContext } from 'react';
+import { SIDEBAR_GROUP } from '@/support/enums/sidebarGroup';
+import { SidebarContext } from '@/contexts/SidebarContext';
 
-const SidebarLinkCollapsible = (props: { title: string; children?: React.ReactNode; icon?: React.ReactNode }) => {
+/**
+ * SidebarLinkCollapsible
+ *
+ * @param title
+ * @param group used to check if the menu is selected, if selectedMenu === group then the menu is opened by default
+ * @param children
+ * @param icon
+ * @param props
+ * @constructor
+ */
+const SidebarLinkCollapsible = (props: {
+    title: string;
+    group: SIDEBAR_GROUP;
+    children?: React.ReactNode;
+    icon?: React.ReactNode;
+}) => {
+    const sidebarContext = useContext(SidebarContext);
     const accordionClass = `${buttonVariants({
         variant: 'sidebar',
     })}  hover:no-underline`;
     const accordionItemClass = 'sidebar-collapsible-accordion border-0 hover:no-underline space-y-2 ml-4';
     const titleClass = props.icon ? 'sidebar-item-text ml-2' : 'sidebar-item-text';
     return (
-        <Accordion type="single" collapsible>
-            <AccordionItem value="item-1" className={accordionItemClass}>
+        <Accordion type="single" collapsible defaultValue={sidebarContext?.selectedMenu}>
+            <AccordionItem value={props.group} className={accordionItemClass}>
                 <AccordionTrigger className={accordionClass}>
                     <div className="sidebar-collapsible-menu">
                         {props.icon}
@@ -24,7 +42,14 @@ const SidebarLinkCollapsible = (props: { title: string; children?: React.ReactNo
     );
 };
 
-const SidebarLinkCollapsibleItem = (props: { routeName: string; title: string; icon?: React.ReactNode }) => {
+const SidebarLinkCollapsibleItem = (props: {
+    group: SIDEBAR_GROUP;
+    routeName: string;
+    title: string;
+    icon?: React.ReactNode;
+}) => {
+    const sidebarContext = useContext(SidebarContext);
+    const handleSetSelectedMenu = () => sidebarContext?.setSelectedMenu(props.group);
     const active = route().current(props.routeName);
     const accordionClass = `${buttonVariants({
         variant: 'sidebar',
@@ -32,11 +57,11 @@ const SidebarLinkCollapsibleItem = (props: { routeName: string; title: string; i
     const linkClass = `${buttonVariants({
         variant: active ? 'sidebar-active' : 'sidebar',
     })} sidebar-collapsible-link`;
-    const titleClass = props.icon ? 'truncate ml-2' : 'truncate ';
+    const titleClass = props.icon ? 'truncate ml-2' : 'truncate';
     return (
         <div className="sidebar-collapsible-item pl-11" title={props.title}>
             <AccordionContent className={accordionClass}>
-                <Link href={route(props.routeName)} className={linkClass}>
+                <Link href={route(props.routeName)} className={linkClass} onClick={handleSetSelectedMenu}>
                     <div className="sidebar-collapsible-icon">{props.icon}</div>
                     <div className={titleClass}>{props.title}</div>
                 </Link>
--- resources/js/Layouts/Partials/Sidebar.tsx
+++ resources/js/Layouts/Partials/Sidebar.tsx
@@ -24,10 +24,12 @@ import { useLocalStorage } from '@uidotdev/usehooks';
 import { useEffect, useRef } from 'react';
 import { STYLING } from '@/support/constants/styling';
 import { ROUTES } from '@/support/constants/routes';
+import { SidebarContext } from '@/contexts/SidebarContext';
+import { SIDEBAR_GROUP } from '@/support/enums/sidebarGroup';
 
 export default function Sidebar() {
     const [sidebarCollapse, setSidebarCollapse] = useLocalStorage('sidebarCollapse', false);
-
+    const [selectedMenu, setSelectedMenu] = useLocalStorage('selectedMenu', '');
     const handleSidebarCollapse = () => {
         setSidebarCollapse(!sidebarCollapse);
     };
@@ -45,109 +47,128 @@ export default function Sidebar() {
     useEffect(applySidebarCollapse, [sidebarCollapse]);
 
     return (
-        <aside ref={sidebarRef} className="sidebar w-72 h-screen border-border border-r-2 transition-all">
-            <nav className="flex flex-col space-y-1">
-                <div className="sidebar-header flex px-4 py-3 border-b-2 h-16">
-                    <img
-                        src="/assets/images/outline putih.png"
-                        alt="logo"
-                        // className=" "
-                        // className="sidebar-header-logo h-full object-contain"
-                        className="sidebar-header-logo h-full "
-                        width={170}
-                        // height={500}
-                    />
-                    <Button
-                        variant="outline"
-                        size="icon"
-                        className="sidebar-collapse-toggle h-10 w-10"
-                        onClick={handleSidebarCollapse}
-                    >
-                        {sidebarCollapse ? (
-                            <RiContractRightLine size={STYLING.ICON.SIZE.SMALL} />
-                        ) : (
-                            <RiContractLeftLine size={STYLING.ICON.SIZE.SMALL} />
-                        )}
-                    </Button>
-                </div>
-                <SidebarMenu title="GENERAL">
-                    <SidebarLink
-                        routeName="dashboard"
-                        title="Dashboard"
-                        icon={<RiHome8Line size={STYLING.ICON.SIZE.SMALL} />}
-                    />
+        <SidebarContext.Provider value={{ selectedMenu, setSelectedMenu }}>
+            <aside ref={sidebarRef} className="sidebar w-72 h-screen border-border border-r-2 transition-all">
+                <nav className="flex flex-col space-y-1">
+                    <div className="sidebar-header flex px-4 py-3 border-b-2 h-16">
+                        <img
+                            src="/assets/images/outline putih.png"
+                            alt="logo"
+                            // className=" "
+                            // className="sidebar-header-logo h-full object-contain"
+                            className="sidebar-header-logo h-full "
+                            width={170}
+                            // height={500}
+                        />
+                        <Button
+                            variant="outline"
+                            size="icon"
+                            className="sidebar-collapse-toggle h-10 w-10"
+                            onClick={handleSidebarCollapse}
+                        >
+                            {sidebarCollapse ? (
+                                <RiContractRightLine size={STYLING.ICON.SIZE.SMALL} />
+                            ) : (
+                                <RiContractLeftLine size={STYLING.ICON.SIZE.SMALL} />
+                            )}
+                        </Button>
+                    </div>
+                    <SidebarMenu title="GENERAL">
+                        <SidebarLink
+                            routeName="dashboard"
+                            title="Dashboard"
+                            icon={<RiHome8Line size={STYLING.ICON.SIZE.SMALL} />}
+                        />
 
-                    <SidebarLinkCollapsible
-                        title="Staff Management"
-                        icon={<RiUser2Line size={STYLING.ICON.SIZE.SMALL} />}
-                    >
-                        {/*division*/}
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.DIVISIONS}.index`}
-                            title="Divisions"
-                            icon={<RiDivideLine size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.WORKSHOPS}.index`}
-                            title="Workshops"
-                            icon={<RiHome2Line size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.WORKSTATIONS}.index`}
-                            title="Workstations"
-                            icon={<RiToolsFill size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.USERS}.index`}
-                            title="Staff"
-                            icon={<RiUserLine size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                    </SidebarLinkCollapsible>
+                        <SidebarLinkCollapsible
+                            group={SIDEBAR_GROUP.STAFF_MANAGEMENT}
+                            title="Staff Management"
+                            icon={<RiUser2Line size={STYLING.ICON.SIZE.SMALL} />}
+                        >
+                            {/*division*/}
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.STAFF_MANAGEMENT}
+                                routeName={`${ROUTES.DIVISIONS}.index`}
+                                title="Divisions"
+                                icon={<RiDivideLine size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.STAFF_MANAGEMENT}
+                                routeName={`${ROUTES.WORKSHOPS}.index`}
+                                title="Workshops"
+                                icon={<RiHome2Line size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.STAFF_MANAGEMENT}
+                                routeName={`${ROUTES.WORKSTATIONS}.index`}
+                                title="Workstations"
+                                icon={<RiToolsFill size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.STAFF_MANAGEMENT}
+                                routeName={`${ROUTES.USERS}.index`}
+                                title="Staff"
+                                icon={<RiUserLine size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                        </SidebarLinkCollapsible>
 
-                    <SidebarLinkCollapsible title="Hak Akses" icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}>
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.PERMISSIONS}.index`}
-                            title="Permissions"
-                            icon={<RiLockUnlockLine size={STYLING.ICON.SIZE.SMALL} />}
+                        <SidebarLinkCollapsible
+                            group={SIDEBAR_GROUP.ACCESS_CONTROL}
+                            title="Hak Akses"
+                            icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}
+                        >
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.ACCESS_CONTROL}
+                                routeName={`${ROUTES.PERMISSIONS}.index`}
+                                title="Permissions"
+                                icon={<RiLockUnlockLine size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.ACCESS_CONTROL}
+                                routeName={`${ROUTES.ROLES}.index`}
+                                title="Roles"
+                                icon={<RiShieldLine size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                        </SidebarLinkCollapsible>
+                    </SidebarMenu>
+                    <SidebarMenu title="MANUFAKTUR" bordered>
+                        <SidebarLink
+                            routeName={`${ROUTES.PROFILE}.edit`}
+                            title="Track Lot"
+                            icon={<ListOrdered size={STYLING.ICON.SIZE.SMALL} />}
                         />
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.ROLES}.index`}
-                            title="Roles"
-                            icon={<RiShieldLine size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                    </SidebarLinkCollapsible>
-                </SidebarMenu>
-                <SidebarMenu title="MANUFAKTUR" bordered>
-                    <SidebarLink
-                        routeName={`${ROUTES.PROFILE}.edit`}
-                        title="Track Lot"
-                        icon={<ListOrdered size={STYLING.ICON.SIZE.SMALL} />}
-                    />
-                    {/* <SidebarLink
+                        {/* <SidebarLink
                         routeName="project"
                         title="Buat Proyek"
                         icon={<ListOrdered size={STYLING.ICON.SIZE.SMALL} />}
                     /> */}
-                    <SidebarLinkCollapsible title="Proyek" icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}>
-                        <SidebarLinkCollapsibleItem
-                            routeName="proyek"
-                            title="List Proyek"
+                        <SidebarLinkCollapsible
+                            group={SIDEBAR_GROUP.PROJECT}
+                            title="Proyek"
                             icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                        <SidebarLinkCollapsibleItem
-                            routeName="buat-proyek"
-                            title="Buat Proyek"
-                            // routeName={`${ROUTES.PROFILE}.edit`}
-                            // title="Order"
-                            icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                        <SidebarLinkCollapsibleItem
-                            routeName={`${ROUTES.PROFILE}.edit`}
-                            title="Track"
-                            icon={<RiFlickrLine size={STYLING.ICON.SIZE.SMALL} />}
-                        />
-                    </SidebarLinkCollapsible>
-                    {/* <SidebarLink
+                        >
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.PROJECT}
+                                routeName="proyek"
+                                title="List Proyek"
+                                icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.PROJECT}
+                                routeName="buat-proyek"
+                                title="Buat Proyek"
+                                // routeName={`${ROUTES.PROFILE}.edit`}
+                                // title="Order"
+                                icon={<RiBox3Line size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                            <SidebarLinkCollapsibleItem
+                                group={SIDEBAR_GROUP.PROJECT}
+                                routeName={`${ROUTES.PROFILE}.edit`}
+                                title="Track"
+                                icon={<RiFlickrLine size={STYLING.ICON.SIZE.SMALL} />}
+                            />
+                        </SidebarLinkCollapsible>
+                        {/* <SidebarLink
                         route={`${ROUTES.PROFILE}.edit"
                         children="Order Batch"
                         icon={<RiBox3Line  size={STYLING.ICON.SIZE.SMALL} />}
@@ -157,21 +178,22 @@ export default function Sidebar() {
                         children="Input Dokumen Pendukung"
                         icon={<RiUserLine  size={STYLING.ICON.SIZE.SMALL} />}
                     /> */}
-                </SidebarMenu>
-                <SidebarMenu title="SUPPORT" bordered>
-                    <SidebarLink
-                        routeName={`${ROUTES.PROFILE}.edit`}
-                        title="Pengaturan"
-                        icon={<RiSettings3Line size={STYLING.ICON.SIZE.SMALL} />}
-                    />
-                    <SidebarLink
-                        routeName={`${ROUTES.PROFILE}.edit`}
-                        title="Helpdesk"
-                        icon={<RiQuestionLine size={STYLING.ICON.SIZE.SMALL} />}
-                    />
-                    <SidebarLogout />
-                </SidebarMenu>
-            </nav>
-        </aside>
+                    </SidebarMenu>
+                    <SidebarMenu title="SUPPORT" bordered>
+                        <SidebarLink
+                            routeName={`${ROUTES.PROFILE}.edit`}
+                            title="Pengaturan"
+                            icon={<RiSettings3Line size={STYLING.ICON.SIZE.SMALL} />}
+                        />
+                        <SidebarLink
+                            routeName={`${ROUTES.PROFILE}.edit`}
+                            title="Helpdesk"
+                            icon={<RiQuestionLine size={STYLING.ICON.SIZE.SMALL} />}
+                        />
+                        <SidebarLogout />
+                    </SidebarMenu>
+                </nav>
+            </aside>
+        </SidebarContext.Provider>
     );
 }
