db79e2059c7973869f8d52315a629509370f7420
N00budrag0n <helmiagung2468@gmail.com>
2024-08-21 10:28:35 +0700

    migration & seeder:  progress + panel

--- app/Http/Resources/ProgressResource.php
+++ app/Http/Resources/ProgressResource.php
@@ -15,6 +15,14 @@ class ProgressResource extends JsonResource {
         return [
             'id' => $this->id,
             'name' => $this->name,
+            // 'status' => $this->status,
+            // 'steps' => StepResource::collection($this->whenLoaded('steps')),
+            // 'steps' => StepResource::collection($this->steps),
+            // 'steps' => $this->steps,
+            // 'completion' => $this->completion,
+            // 'worker' => new UserResource($this->whenLoaded('worker')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
