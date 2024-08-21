cd5ceaacdb75ec1dd914dc0a6a0c9187d74b6bfe
I <91296724+i9191@users.noreply.github.com>
2024-08-21 12:45:42 +0700

    Seed and resource for components

---
+++ app/Models/Component.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Component extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'progress_id',
+        'panel_id',
+    ];
+}
---
+++ database/seeders/ComponentSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Carriage;
+use App\Models\Component;
+use App\Models\Panel;
+use App\Models\Progress;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComponentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (file_exists(base_path('database/data/component.csv'))) {
+            $csvData = array_map('str_getcsv', file(base_path('database/data/component.csv')));
+            array_walk($csvData, function (&$a) use ($csvData) {
+                $a = array_map(function ($value) {
+                    return $value !== '' ? $value : null;
+                }, array_combine($csvData[0], $a));
+            });
+            array_shift($csvData); // remove column header
+            foreach ($csvData as $data) {
+                if (Progress::where('name','LIKE', '%' . $data['panel_name']. '%')->where('name','LIKE', '%' . $data['work_aspect']. '%')->exists() && Panel::where('name','LIKE', '%' . $data['panel_name']. '%')->whereCarriageId(Carriage::whereType($data['car_type'])->first()->id)->exists()) {
+                    Component::create([
+                        'name' => $data['component'],
+                        'progress_id' => Progress::where('name','LIKE', '%' . $data['panel_name']. '%')->where('name','LIKE', '%' . $data['work_aspect']. '%')->first()->id,
+                        'panel_id' => Panel::where('name','LIKE', '%' . $data['panel_name']. '%')->whereCarriageId(Carriage::whereType($data['car_type'])->first()->id)->first()->id,
+                    ]);
+                }
+            }
+        }
+    }
+}
