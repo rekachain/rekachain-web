ed947eb4b32f2bccd51c134f995e0d510b6c0f94
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-08-26 10:28:47 +0700

    add division component

--- resources/js/Pages/Division/Partials/Divisions.tsx
+++ resources/js/Pages/Division/Partials/Divisions.tsx
@@ -9,6 +9,8 @@ import GenericPagination from '@/Components/GenericPagination';
 import { ServiceFilterOptions } from '@/support/interfaces/others/ServiceFilterOptions';
 import { useConfirmation } from '@/hooks/useConfirmation';
 import { divisionService } from '@/services/divisionService';
+import { useMediaQuery } from 'react-responsive';
+import AnimateIn from '@/lib/AnimateIn';
 
 export default function () {
     const [divisionResponse, setDivisionResponse] = useState<PaginateResponse<DivisionResource>>();
@@ -43,37 +45,96 @@ export default function () {
     const handlePageChange = (page: number) => {
         setFilters({ ...filters, page });
     };
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 900px)' });
+
+    const isDesktopOrLaptop = useMediaQuery({
+        query: '(min-width: 900px)',
+    });
 
     return (
         <div className="space-y-4">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Nama</TableHead>
-                        <TableHead></TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
+            {isTabletOrMobile && (
+                <>
                     {divisionResponse?.data.map(division => (
-                        <TableRow key={division.id}>
-                            <TableCell>{division.name}</TableCell>
-                            <TableCell>
-                                <Link
-                                    className={buttonVariants({ variant: 'link' })}
-                                    href={route(`${ROUTES.DIVISIONS}.edit`, division.id)}
-                                >
-                                    Edit
-                                </Link>
-                                <Button variant="link" onClick={() => handleDivisionDeletion(division.id)}>
-                                    Delete
-                                </Button>
-                            </TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                        <AnimateIn
+                            from="opacity-0 -translate-y-4"
+                            to="opacity-100 translate-y-0 translate-x-0"
+                            duration={300}
+                        >
+                            <div
+                                key={division.id}
+                                className="border-black dark:border-white border-2 rounded-md p-2 flex flex-col gap-2"
+                            >
+                                <div className="flex w-full justify-between items-scenter">
+                                    <h4 className="font-bold text-xl">{division.name}</h4>
+                                    <div className="text-center">
+                                        {/* <h5 className="font-bold text-xs items-center "> {division.role?.name}</h5> */}
+                                    </div>
+                                </div>
 
-            <GenericPagination meta={divisionResponse?.meta} handleChangePage={handlePageChange} />
+                                {/* <h5 className="font-bold text-sm ">NIP : {division.nip}</h5>
+                                <div className="flex">
+                                    <div className="">
+                                        <p className="text-xs">Email </p>
+                                        <p className="text-xs">No.Hp</p>
+                                    </div>
+                                    <div className="pl-4">
+                                        <p className="text-xs">: </p>
+                                        <p className="text-xs">: </p>
+                                    </div>
+                                    <div className="pl-4 ">
+                                        <p className="text-xs">{division.email}</p>
+                                        <p className="text-xs">{division.phone_number}</p>
+                                    </div> */}
+                                <div className="flex items-center justify-end w-full">
+                                    <Link
+                                        className={buttonVariants({ variant: 'link' })}
+                                        href={route(`${ROUTES.DIVISIONS}.edit`, division.id)}
+                                    >
+                                        Edit
+                                    </Link>
+                                    <Button variant="link" onClick={() => handleDivisionDeletion(division.id)}>
+                                        Delete
+                                    </Button>
+                                </div>
+                            </div>
+                            {/* </div> */}
+                        </AnimateIn>
+                    ))}
+                </>
+            )}
+            {isDesktopOrLaptop && (
+                <div className="">
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead>Nama</TableHead>
+                                <TableHead></TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {divisionResponse?.data.map(division => (
+                                <TableRow key={division.id}>
+                                    <TableCell>{division.name}</TableCell>
+                                    <TableCell>
+                                        <Link
+                                            className={buttonVariants({ variant: 'link' })}
+                                            href={route(`${ROUTES.DIVISIONS}.edit`, division.id)}
+                                        >
+                                            Edit
+                                        </Link>
+                                        <Button variant="link" onClick={() => handleDivisionDeletion(division.id)}>
+                                            Delete
+                                        </Button>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+
+                    <GenericPagination meta={divisionResponse?.meta} handleChangePage={handlePageChange} />
+                </div>
+            )}
         </div>
     );
 }
--- resources/js/Pages/User/Partials/Users.tsx
+++ resources/js/Pages/User/Partials/Users.tsx
@@ -11,6 +11,7 @@ import GenericPagination from '@/Components/GenericPagination';
 import { ServiceFilterOptions } from '@/support/interfaces/others/ServiceFilterOptions';
 import { useConfirmation } from '@/hooks/useConfirmation';
 import { useMediaQuery } from 'react-responsive';
+import AnimateIn from '@/lib/AnimateIn';
 
 export default function () {
     const [userResponse, setUserResponse] = useState<PaginateResponse<UserResource>>();
@@ -56,49 +57,60 @@ export default function () {
         query: '(min-width: 900px)',
     });
     return (
-        <div className="space-y-4">
+        <div className="space-y-4 px-2">
             {isTabletOrMobile && (
                 <>
-                    {userResponse?.data.map(user => (
-                        <div
-                            key={user.id}
-                            className="border-black dark:border-white border-2 rounded-md p-2 flex flex-col gap-2"
-                        >
-                            <div className="flex w-full justify-between items-scenter">
-                                <h4 className="font-bold text-xl">{user.name}</h4>
-                                <div className="text-center">
-                                    <h5 className="font-bold text-xs items-center "> {user.role?.name}</h5>
-                                </div>
-                            </div>
-
-                            <h5 className="font-bold text-sm ">NIP : {user.nip}</h5>
-                            <div className="flex">
-                                <div className="">
-                                    <p className="text-xs">Email </p>
-                                    <p className="text-xs">No.Hp</p>
-                                </div>
-                                <div className="pl-4">
-                                    <p className="text-xs">: </p>
-                                    <p className="text-xs">: </p>
-                                </div>
-                                <div className="pl-4 ">
-                                    <p className="text-xs">{user.email}</p>
-                                    <p className="text-xs">{user.phone_number}</p>
-                                </div>
-                                {user.id !== auth.user.id &&
-                                    (auth.user.role === 'Super Admin' || user.role.name !== 'Super Admin') && (
-                                        <div className="flex items-center justify-end w-full">
-                                            <Link href={route(`${ROUTES.USERS}.edit`, user.id)}>
-                                                <p className="text-xs">Edit</p>
-                                            </Link>
-                                            <Button variant={'link'} onClick={() => handleUserDeletion(user.id)}>
-                                                <p className="text-xs">Delete</p>
-                                            </Button>
+                    {userResponse?.data.map(user => {
+                        return (
+                            <AnimateIn
+                                from="opacity-0 -translate-y-4"
+                                to="opacity-100 translate-y-0 translate-x-0"
+                                duration={300}
+                            >
+                                <div
+                                    key={user.id}
+                                    className="border-black dark:border-white border-2 rounded-md p-2 flex flex-col gap-2"
+                                >
+                                    <div className="flex w-full justify-between items-scenter">
+                                        <h4 className="font-bold text-xl">{user.name}</h4>
+                                        <div className="text-center">
+                                            <h5 className="font-bold text-xs items-center "> {user.role?.name}</h5>
                                         </div>
-                                    )}
-                            </div>
-                        </div>
-                    ))}
+                                    </div>
+
+                                    <h5 className="font-bold text-sm ">NIP : {user.nip}</h5>
+                                    <div className="flex">
+                                        <div className="">
+                                            <p className="text-xs">Email </p>
+                                            <p className="text-xs">No.Hp</p>
+                                        </div>
+                                        <div className="pl-4">
+                                            <p className="text-xs">: </p>
+                                            <p className="text-xs">: </p>
+                                        </div>
+                                        <div className="pl-4 ">
+                                            <p className="text-xs">{user.email}</p>
+                                            <p className="text-xs">{user.phone_number}</p>
+                                        </div>
+                                        {user.id !== auth.user.id &&
+                                            (auth.user.role === 'Super Admin' || user.role.name !== 'Super Admin') && (
+                                                <div className="flex items-center justify-end w-full">
+                                                    <Link href={route(`${ROUTES.USERS}.edit`, user.id)}>
+                                                        <p className="text-xs">Edit</p>
+                                                    </Link>
+                                                    <Button
+                                                        variant={'link'}
+                                                        onClick={() => handleUserDeletion(user.id)}
+                                                    >
+                                                        <p className="text-xs">Delete</p>
+                                                    </Button>
+                                                </div>
+                                            )}
+                                    </div>
+                                </div>
+                            </AnimateIn>
+                        );
+                    })}
                     <GenericPagination meta={userResponse?.meta} handleChangePage={handlePageChange} />
                 </>
             )}
