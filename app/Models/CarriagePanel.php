bb490e451e14afc0453eb21d2c009afa04ab9199
Galur Arasy <adamhenderson3x3@gmail.com>
2024-08-30 00:25:44 +0700

    refactor: replace CarriagePanel relationship; components with carriage_panel_components

--- app/Models/CarriagePanel.php
+++ app/Models/CarriagePanel.php
@@ -26,7 +26,7 @@ class CarriagePanel extends Model {
         return $this->belongsTo(Panel::class);
     }
 
-    public function components(): HasMany {
+    public function carriage_panel_components(): HasMany {
         return $this->hasMany(CarriagePanelComponent::class);
     }
 }
--- app/Services/CarriagePanelService.php
+++ app/Services/CarriagePanelService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\BaseCrudService;
+use App\Models\CarriagePanel;
 use App\Support\Interfaces\Repositories\CarriagePanelRepositoryInterface;
 use App\Support\Interfaces\Services\CarriagePanelComponentServiceInterface;
 use App\Support\Interfaces\Services\CarriagePanelServiceInterface;
@@ -16,8 +17,13 @@ class CarriagePanelService extends BaseCrudService implements CarriagePanelServi
         return CarriagePanelRepositoryInterface::class;
     }
 
+    /**
+     * @param  int|CarriagePanel  $keyOrModel
+     *
+     * @throws \Exception
+     */
     public function delete($keyOrModel): bool {
-        $keyOrModel->components()->each(function ($component) {
+        $keyOrModel->carriage_panel_components()->each(function ($component) {
             $this->carriagePanelComponentService->delete($component);
         });
 
