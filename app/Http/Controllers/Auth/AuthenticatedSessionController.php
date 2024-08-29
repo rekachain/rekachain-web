a9c6c232feecd4cbd7aefd5b6672e1bbb415ebc4
Galur Arasy <adamhenderson3x3@gmail.com>
2024-08-30 01:01:58 +0700

    feature: restrict unauthorized specific roles

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
+use App\Support\Enums\RoleEnum;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -30,6 +31,25 @@ class AuthenticatedSessionController extends Controller {
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        $user = Auth::user();
+        $allowedRoles = [
+            RoleEnum::SUPER_ADMIN->value,
+            RoleEnum::PPC_PERENCANAAN->value,
+            RoleEnum::PPC_PENGENDALIAN->value,
+            RoleEnum::SUPERVISOR_MEKANIK->value,
+            RoleEnum::SUPERVISOR_ELEKTRIK->value,
+            RoleEnum::SUPERVISOR_ASSEMBLY->value,
+        ];
+
+        if (!in_array($user->roles()->first()->name, $allowedRoles)) {
+            Auth::guard('web')->logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            // TODO: login_error doesnt show, might check this later
+            return redirect('/')->withErrors(['login_error' => __('validation.custom.auth.unauthorized')]);
+        }
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
