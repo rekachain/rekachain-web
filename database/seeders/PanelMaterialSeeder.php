11811506411955e69cae91737e753ffd17ba2a67
Galur Arasy <adamhenderson3x3@gmail.com>
2024-09-04 09:20:55 +0700

    update: ApiPanelAttachment index resource intent

--- database/seeders/PanelMaterialSeeder.php
+++ database/seeders/PanelMaterialSeeder.php
@@ -3,20 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\PanelMaterial;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Database\Seeders\Helpers\CsvReader;
 use Illuminate\Database\Seeder;
 
-class PanelMaterialSeeder extends Seeder
-{
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PanelMaterialSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         $csvReader = new CsvReader('panel_material');
+
         $csvData = $csvReader->getCsvData();
 
+        if (!$csvData) {
+            PanelMaterial::factory(10)->create();
+
+            return;
+        }
         foreach ($csvData as $row) {
             PanelMaterial::factory()->create($row);
         }
