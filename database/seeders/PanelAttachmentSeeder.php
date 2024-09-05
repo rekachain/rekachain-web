9a58ccd5fe7bad3638e49acc69aee84a73438e43
I <91296724+i9191@users.noreply.github.com>
2024-09-05 09:59:41 +0700

    Add attachment number and qr code on panel attachment

--- database/seeders/PanelAttachmentSeeder.php
+++ database/seeders/PanelAttachmentSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Helpers\NumberHelper;
 use App\Models\PanelAttachment;
 use Database\Seeders\Helpers\CsvReader;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PanelAttachmentSeeder extends Seeder
@@ -19,7 +19,15 @@ class PanelAttachmentSeeder extends Seeder
 
         if ($csvData) {
             foreach ($csvData as $data) {
-                PanelAttachment::create($data);
+                $panelAttcahment = PanelAttachment::create($data);
+                $panelAttachments = PanelAttachment::whereCarriageTrainsetId($panelAttcahment->carriage_trainset_id)->get();
+                $i = $panelAttachments->where('id', '<', $panelAttcahment->id)->count() + 1;
+                $panelAttcahment->update([
+                    'attachment_number' => $panelAttcahment->id . '/PPC/KPM/' . NumberHelper::intToRoman($i) . '/' . date('Y', strtotime($panelAttcahment->created_at)),
+                ]);
+                $panelAttcahment->update([
+                    'qr_code' => 'KPM:' . $panelAttcahment->attachment_number . ';P:'. $panelAttcahment->carriage_trainset->trainset->project->name . ';TS:' . $panelAttcahment->carriage_trainset->trainset->name . ';;'
+                ]);
             }
         } else {
             PanelAttachment::factory(1)->create();
