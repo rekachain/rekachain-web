56be8736e48fc11c5d0c0a8a8614793f44cbf7b2
N00budrag0n <helmiagung2468@gmail.com>
2024-09-05 08:58:24 +0700

    test: api trainset test

--- tests/Feature/Http/Controllers/Api/ApiAuthControllerTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-test('view all Components', function () {
-    createComponent();
-    actAsSuperAdmin()->get('/api/components')->assertStatus(200);
-});
--- tests/Feature/Http/Controllers/Api/ApiTrainsetControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ApiTrainsetControllerTest.php
@@ -1,7 +1,42 @@
 <?php
 
 
-test('view all Components', function () {
-    createComponent();
-    actAsSuperAdmin()->get('/api/components')->assertStatus(200);
+test('view all trainsets', function () {
+    createTrainset();
+    actAsSuperAdmin()->get('/api/trainsets')->assertStatus(200);
 });
+
+test('view one trainset', function () {
+    $trainset = createTrainset();
+    actAsSuperAdmin()->get('/api/trainsets/' . $trainset->id)->assertStatus(200);
+});
+
+test('store trainset', function () {
+    $project = createProject();
+    actAsSuperAdmin()->post('/api/trainsets', [
+        'name' => 'test',
+        'project_id' => $project->id,
+    ])->assertStatus(201);
+});
+
+test('edit trainset', function () {
+    $trainset = createTrainset();
+    actAsSuperAdmin()->put('/api/trainsets/' . $trainset->id, [
+        'name' => 'test',
+    ])->assertStatus(200);
+
+    $this->assertDatabaseHas('trainsets', [
+        'id' => $trainset->id,
+        'name' => 'test',
+    ]);
+});
+
+// NOT IMPLEMENTED YET
+// test('destroy trainset', function () {
+//     $trainset = createTrainset();
+//     actAsSuperAdmin()->delete('/api/trainsets/' . $trainset->id)->assertStatus(200);
+
+//     $this->assertDatabaseMissing('trainsets', [
+//         'id' => $trainset->id,
+//     ]);
+// });
