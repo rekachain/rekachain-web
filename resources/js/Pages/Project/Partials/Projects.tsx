64e39a7da6ade11d551e72dab819175b3e8ccc2d
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-09-05 10:55:54 +0700

    add project table view

--- resources/js/Pages/Project/Partials/Projects.tsx
+++ resources/js/Pages/Project/Partials/Projects.tsx
@@ -1,4 +1,3 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/ui/table';
 import { Link } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 import { ProjectResource } from '@/support/interfaces/resources';
@@ -12,6 +11,7 @@ import { projectService } from '@/services/projectService';
 import { useLoading } from '@/contexts/LoadingContext';
 import { useSuccessToast } from '@/hooks/useToast';
 import ProjectCardView from './Partials/ProjectCardView';
+import ProjectTableView from './Partials/ProjectTableView';
 
 export default function () {
     const [projectResponse, setProjectResponse] = useState<PaginateResponse<ProjectResource>>();
@@ -52,47 +52,11 @@ export default function () {
                 {projectResponse && (
                     <>
                         <div className="hidden md:block">
-                            <Table>
-                                <TableHeader>
-                                    <TableRow>
-                                        <TableHead>Nama</TableHead>
-                                        <TableHead>Tanggal Inisiasi</TableHead>
-                                        <TableHead>Jumlah Trainset</TableHead>
-                                        <TableHead></TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {projectResponse?.data.map(project => (
-                                        <TableRow key={project.id}>
-                                            <TableCell>{project.name}</TableCell>
-                                            <TableCell>{project.initial_date}</TableCell>
-                                            <TableCell>{project.trainset_count}</TableCell>
-                                            <TableCell>
-                                                <Link
-                                                    className={buttonVariants({ variant: 'link' })}
-                                                    href={route(`${ROUTES.PROJECTS}.edit`, project.id)}
-                                                >
-                                                    Edit
-                                                </Link>
-                                                {project.can_be_deleted && (
-                                                    <Button
-                                                        variant="link"
-                                                        onClick={() => handleProjectDeletion(project.id)}
-                                                    >
-                                                        Delete
-                                                    </Button>
-                                                )}
-                                                <Link
-                                                    className={buttonVariants({ variant: 'link' })}
-                                                    href={route(`${ROUTES.PROJECTS_TRAINSETS}.index`, project.id)}
-                                                >
-                                                    Trainset
-                                                </Link>
-                                            </TableCell>
-                                        </TableRow>
-                                    ))}
-                                </TableBody>
-                            </Table>
+                            <ProjectTableView
+                                projectResponse={projectResponse}
+                                handleProjectDeletion={handleProjectDeletion}
+                                auth={''}
+                            ></ProjectTableView>
                         </div>
 
                         <div className="block md:hidden">
