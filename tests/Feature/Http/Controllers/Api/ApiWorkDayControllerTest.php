37f3a0d86d8df016b87f16184057391a4c060ccd
N00budrag0n <helmiagung2468@gmail.com>
2024-09-05 13:46:56 +0700

    test: api workdays, workdaytimes

--- tests/Feature/Http/Controllers/Api/ApiWorkDayControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ApiWorkDayControllerTest.php
@@ -1,7 +1,42 @@
 <?php
 
 
-test('view all Components', function () {
-    createComponent();
-    actAsSuperAdmin()->get('/api/components')->assertStatus(200);
+test('view all work-days', function () {
+    createWorkDay();
+    actAsSuperAdmin()->get('/api/work-days')->assertStatus(200);
+});
+
+test('view work-day', function () {
+    $workday = createWorkDay();
+    actAsSuperAdmin()->get('/api/work-days/' . $workday->id)->assertStatus(200);
+});
+
+test('store work-day', function () {
+    actAsSuperAdmin()->post('/api/work-days', [
+        'day' => 'Test Day'
+    ])->assertStatus(201);
+
+    $this->assertDatabaseHas('work_days', [
+        'day' => 'Test Day'
+    ]);
+});
+
+test('update work-day', function () {
+    $workday = createWorkDay();
+    actAsSuperAdmin()->put('/api/work-days/' . $workday->id, [
+        'day' => 'Test Day'
+    ])->assertStatus(200);
+
+    $this->assertDatabaseHas('work_days', [
+        'id' => $workday->id,
+        'day' => 'Test Day'
+    ]);
+});
+
+test('destroy work-day', function () {
+    $workday = createWorkDay();
+    actAsSuperAdmin()->delete('/api/work-days/' . $workday->id)->assertStatus(200);
+    $this->assertDatabaseMissing('work_days', [
+        'id' => $workday->id
+    ]);
 });
