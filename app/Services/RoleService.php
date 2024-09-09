eb94b7b4d64e839c2f3c437564734ada5dba8eb8
Galur Arasy <adamhenderson3x3@gmail.com>
2024-09-09 10:31:12 +0700

    fix: remove stale permission cache when updating role permission

--- app/Services/RoleService.php
+++ app/Services/RoleService.php
@@ -5,8 +5,18 @@ namespace App\Services;
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\BaseCrudService;
 use App\Support\Interfaces\Repositories\RoleRepositoryInterface;
 use App\Support\Interfaces\Services\RoleServiceInterface;
+use Illuminate\Database\Eloquent\Model;
 
 class RoleService extends BaseCrudService implements RoleServiceInterface {
+    public function update($keyOrModel, array $data): ?Model {
+        if (isset($data['permissions'])) {
+            $keyOrModel->permissions()->sync($data['permissions']);
+            app()->make(\Spatie\Permission\PermissionRegistrar::class)->forgetCachedPermissions();
+        }
+
+        return parent::update($keyOrModel, $data);
+    }
+
     protected function getRepositoryClass(): string {
         return RoleRepositoryInterface::class;
     }
