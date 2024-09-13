c49bfce598700e06010829afad9d7964956398c5
Galur Arasy <adamhenderson3x3@gmail.com>
2024-09-13 08:25:16 +0700

    update!: fix trainset change preset when theres attachments, add trainset status

--- database/seeders/PanelAttachmentSeeder.php
+++ database/seeders/PanelAttachmentSeeder.php
@@ -4,16 +4,15 @@ namespace Database\Seeders;
 
 use App\Helpers\NumberHelper;
 use App\Models\PanelAttachment;
+use App\Support\Enums\TrainsetStatusEnum;
 use Database\Seeders\Helpers\CsvReader;
 use Illuminate\Database\Seeder;
 
-class PanelAttachmentSeeder extends Seeder
-{
+class PanelAttachmentSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
+    public function run(): void {
         $csvReader = new CsvReader('panel_attachment');
         $csvData = $csvReader->getCsvData();
 
@@ -25,6 +24,7 @@ class PanelAttachmentSeeder extends Seeder
                 $panelAttachment->update([
                     'attachment_number' => $panelAttachment->id . '/PPC/KPM/' . NumberHelper::intToRoman($i) . '/' . date('Y', strtotime($panelAttachment->created_at)),
                 ]);
+                $panelAttachment->trainset()->update(['status' => TrainsetStatusEnum::PROGRESS->value]);
             }
         } else {
             PanelAttachment::factory(1)->create();
