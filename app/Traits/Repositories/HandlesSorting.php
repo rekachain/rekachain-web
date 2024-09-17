fea0b4942b902f8daf34441169c56fd03716c175
Galur Arasy <adamhenderson3x3@gmail.com>
2024-09-17 09:13:40 +0700

    draft: handle multiple ordering parameters

--- app/Traits/Repositories/HandlesSorting.php
+++ app/Traits/Repositories/HandlesSorting.php
@@ -10,8 +10,17 @@ trait HandlesSorting {
      */
     protected function applySorting(Builder $query, array $searchParams = []): Builder {
         if (isset($searchParams['orderBy'])) {
-            $query->orderBy($searchParams['orderBy'], $searchParams['sortBy'] ?? 'desc');
+            $query->orderBy($searchParams['orderBy'], $searchParams['sortDirection'] ?? 'desc');
         }
+
+        //        Draft: Multiple ordering case
+        //        If the request contains an 'ordering' parameter, apply it.
+        //
+        //        if (isset($searchParams['ordering']) && is_array($searchParams['ordering'])) {
+        //            foreach ($searchParams['ordering'] as $field => $direction) {
+        //                $query->orderBy($field, $direction);
+        //            }
+        //        }
 
         return $query;
     }
