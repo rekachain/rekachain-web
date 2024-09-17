71b327d934fe38057340625cd72aaceba397e615
ChamzalIsal <hhamzalnur10@gmail.com>
2024-09-17 08:16:56 +0700

    feature: add seeder ProgressStep

--- database/seeders/ProgressStepSeeder.php
+++ database/seeders/ProgressStepSeeder.php
@@ -2,7 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\ProgressStep;
+use Database\Seeders\Helpers\CsvReader;
 use Illuminate\Database\Seeder;
 
 class ProgressStepSeeder extends Seeder
@@ -10,8 +11,16 @@ class ProgressStepSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
-    {
-        //
+    public function run(): void {
+        $csvReader = new CsvReader('progress_step');
+        $csvData = $csvReader->getCsvData();
+
+        if ($csvData) {
+            foreach ($csvData as $data) {
+                ProgressStep::create($data);
+            }
+        } else {
+            ProgressStep::factory(10)->create();
+        }
     }
 }
