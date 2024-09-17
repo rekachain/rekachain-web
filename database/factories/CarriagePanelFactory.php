78d1ee711aec6dbbd22148bdcea651d4a0c40a85
N00budrag0n <helmiagung2468@gmail.com>
2024-09-17 13:28:53 +0700

    test: web CarriagePanel add & adjust

--- database/factories/CarriagePanelFactory.php
+++ database/factories/CarriagePanelFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Panel;
+use App\Models\Progress;
+use App\Models\CarriageTrainset;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -15,6 +18,10 @@ class CarriagePanelFactory extends Factory {
      */
     public function definition(): array {
         return [
+            'progress_id' => Progress::inRandomOrder()->first()->id,
+            'carriage_trainset_id' => CarriageTrainset::inRandomOrder()->first()->id,
+            'panel_id' => Panel::inRandomOrder()->first()->id,
+            'qty' => $this->faker->numberBetween(1, 10),
             // TODO: Define factory attributes
         ];
     }
--- tests/Feature/Http/Controllers/CarriagePanelControllerTest.php
+++ tests/Feature/Http/Controllers/CarriagePanelControllerTest.php
@@ -1,105 +1,133 @@
 <?php
 
-// use App\Models\User;
-// use App\Models\CarriagePanel;
+use App\Models\User;
+use App\Models\Panel;
+use App\Models\Progress;
+use App\Models\CarriagePanel;
+use App\Models\CarriageTrainset;
 
-// test('index method returns paginated carriage-panels', function () {
-//     $user = User::factory()->create();
-//     CarriagePanel::factory()->count(5)->create();
+test('index method returns paginated carriage-panels', function () {
+    $user = User::factory()->superAdmin()->create();
+    createProgress();
+    createPanel();
+    createCarriageTrainset();
+    CarriagePanel::factory()->count(5)->create();
 
-//     $response = $this->actingAs($user)->getJson('/carriage-panels?page=1&perPage=5');
+    $response = $this->actingAs($user)->getJson('/carriage-panels?page=1&perPage=5');
 
-//     $response->assertStatus(200)
-//         ->assertJsonStructure(['data', 'meta'])
-//         ->assertJsonCount(5, 'data');
-// });
+    $response->assertStatus(200)
+        ->assertJsonStructure(['data', 'meta'])
+        ->assertJsonCount(5, 'data');
+});
 
-// // test('create method returns create page', function () {
-// //     $user = User::factory()->create();
+// test('create method returns create page', function () {
+//     $user = User::factory()->superAdmin()->create();
 
-// //     $response = $this->actingAs($user)->get('/carriage-panels/create');
-
-// //     $response->assertStatus(200)
-// //         ->assertInertia(fn ($assert) => $assert->component('CarriagePanel/Create'));
-// // });
-
-// test('store method creates new carriagePanel', function () {
-//     $user = User::factory()->create();
-//     $carriagePanelData = [
-//         'day' => 'Test day',
-//     ];
-
-//     $response = $this->actingAs($user)->postJson('/carriage-panels', $carriagePanelData);
-
-//     $response->assertStatus(201)
-//         ->assertJsonStructure(['id', 'day']);
-//     $this->assertDatabaseHas('carriage_panels', $carriagePanelData);
-// });
-
-// // NOT IMPLEMENTED
-// // test('store method imports carriage-panels', function () {
-// //     Storage::fake('local');
-// //     $user = User::factory()->create();
-// //     $file = UploadedFile::fake()->create('carriage-panels.xlsx');
-
-// //     $response = $this->actingAs($user)->postJson('/carriage-panels', [
-// //         'intent' => IntentEnum::WEB_CARRIAGE_IMPORT_CARRIAGE->value,
-// //         'import_file' => $file,
-// //     ]);
-
-// //     $response->assertStatus(204);
-// // });
-
-// test('show method returns carriagePanel details', function () {
-//     $user = User::factory()->create();
-//     $carriagePanel = CarriagePanel::factory()->create();
-
-//     $response = $this->actingAs($user)->getJson("/carriage-panels/{$carriagePanel->id}");
+//     $response = $this->actingAs($user)->get('/carriage-panels/create');
 
 //     $response->assertStatus(200)
-//         ->assertJson(['id' => $carriagePanel->id, 'day' => $carriagePanel->day]);
+//         ->assertInertia(fn ($assert) => $assert->component('CarriagePanel/Create'));
 // });
 
-// // test('edit method returns edit page', function () {
-// //     $user = User::factory()->create();
-// //     $carriagePanel = CarriagePanel::factory()->create();
+test('store method creates new carriagePanel', function () {
+    $user = User::factory()->superAdmin()->create();
+    createProgress();
+    createPanel();
+    createCarriageTrainset();
+    dump(CarriageTrainset::inRandomOrder()->first()->id);
+    $carriagePanelData = [
+        'progress_id' => Progress::inRandomOrder()->first()->id,
+        'carriage_trainset_id' => CarriageTrainset::inRandomOrder()->first()->id,
+        'panel_id' => Panel::inRandomOrder()->first()->id,
+        'qty' => 1,
+    ];
 
-// //     $response = $this->actingAs($user)->get("/carriage-panels/{$carriagePanel->id}/edit");
+    $response = $this->actingAs($user)->postJson('/carriage-panels', $carriagePanelData);
 
-// //     $response->assertStatus(200)
-// //         ->assertInertia(fn ($assert) => $assert->component('CarriagePanel/Edit'));
-// // });
+    $response->assertStatus(201)
+        ->assertJsonStructure(['id', 'progress_id', 'carriage_trainset_id', 'panel_id', 'qty']);
+    $this->assertDatabaseHas('carriage_panels', $carriagePanelData);
+});
 
-// test('update method updates carriagePanel', function () {
-//     $user = User::factory()->create();
+// NOT IMPLEMENTED
+// test('store method imports carriage-panels', function () {
+//     Storage::fake('local');
+//     $user = User::factory()->superAdmin()->create();
+//     $file = UploadedFile::fake()->create('carriage-panels.xlsx');
+
+//     $response = $this->actingAs($user)->postJson('/carriage-panels', [
+//         'intent' => IntentEnum::WEB_CARRIAGE_IMPORT_CARRIAGE->value,
+//         'import_file' => $file,
+//     ]);
+
+//     $response->assertStatus(204);
+// });
+
+test('show method returns carriagePanel details', function () {
+    $user = User::factory()->superAdmin()->create();
+    createProgress();
+    createPanel();
+    createCarriageTrainset();
+    $carriagePanel = CarriagePanel::factory()->create();
+
+    $response = $this->actingAs($user)->getJson("/carriage-panels/{$carriagePanel->id}");
+
+    $response->assertStatus(200)
+        ->assertJson([
+            'id' => $carriagePanel->id,
+            'progress_id' => $carriagePanel->progress_id,
+            'carriage_trainset_id' => $carriagePanel->carriage_trainset_id,
+            'panel_id' => $carriagePanel->panel_id,
+            'qty' => $carriagePanel->qty,
+        ]);
+});
+
+// test('edit method returns edit page', function () {
+//     $user = User::factory()->superAdmin()->create();
 //     $carriagePanel = CarriagePanel::factory()->create();
-//     $updatedData = [
-//         'day' => 'Updated Day',
-//     ];
 
-//     $response = $this->actingAs($user)->putJson("/carriage-panels/{$carriagePanel->id}", $updatedData);
+//     $response = $this->actingAs($user)->get("/carriage-panels/{$carriagePanel->id}/edit");
 
 //     $response->assertStatus(200)
-//         ->assertJson($updatedData);
-//     $this->assertDatabaseHas('carriage_panels', $updatedData);
+//         ->assertInertia(fn ($assert) => $assert->component('CarriagePanel/Edit'));
 // });
 
-// test('destroy method deletes carriagePanel', function () {
-//     $user = User::factory()->create();
-//     $carriagePanel = CarriagePanel::factory()->create();
+test('update method updates carriagePanel', function () {
+    $user = User::factory()->superAdmin()->create();
+    createProgress();
+    createPanel();
+    createCarriageTrainset();
+    $carriagePanel = CarriagePanel::factory()->create();
+    $updatedData = [
+        'qty' => 2,
+    ];
 
-//     $response = $this->actingAs($user)->deleteJson("/carriage-panels/{$carriagePanel->id}");
+    $response = $this->actingAs($user)->putJson("/carriage-panels/{$carriagePanel->id}", $updatedData);
 
-//     $response->assertStatus(200);
-//     $this->assertDatabaseMissing('carriage_panels', ['id' => $carriagePanel->id]);
+    $response->assertStatus(200)
+        ->assertJson($updatedData);
+    $this->assertDatabaseHas('carriage_panels', $updatedData);
+});
+
+test('destroy method deletes carriagePanel', function () {
+    $user = User::factory()->superAdmin()->create();
+    createProgress();
+    createPanel();
+    createCarriageTrainset();
+    $carriagePanel = CarriagePanel::factory()->create();
+
+    $response = $this->actingAs($user)->deleteJson("/carriage-panels/{$carriagePanel->id}");
+
+    $response->assertStatus(204);
+    $this->assertDatabaseMissing('carriage_panels', ['id' => $carriagePanel->id]);
+});
+
+// NOT IMPLEMENTED
+// test('index method returns import template', function () {
+//     $user = User::factory()->superAdmin()->create();
+
+//     $response = $this->actingAs($user)->getJson('/carriage-panels?intent=' . IntentEnum::WEB_CARRIAGE_GET_TEMPLATE_IMPORT_CARRIAGE->value);
+
+//     $response->assertStatus(200)
+//         ->assertDownload('carriage-panels_template.xlsx');
 // });
-
-// // NOT IMPLEMENTED
-// // test('index method returns import template', function () {
-// //     $user = User::factory()->create();
-
-// //     $response = $this->actingAs($user)->getJson('/carriage-panels?intent=' . IntentEnum::WEB_CARRIAGE_GET_TEMPLATE_IMPORT_CARRIAGE->value);
-
-// //     $response->assertStatus(200)
-// //         ->assertDownload('carriage-panels_template.xlsx');
-// // });
