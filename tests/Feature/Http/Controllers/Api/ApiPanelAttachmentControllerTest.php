7289b88ea157c2eec6a8290564f2c277e730ebe8
N00budrag0n <helmiagung2468@gmail.com>
2024-09-18 14:42:37 +0700

    test: api ApiPanelAttachmentControllerTest.php add 50%

--- tests/Feature/Http/Controllers/Api/ApiPanelAttachmentControllerTest.php
+++ tests/Feature/Http/Controllers/Api/ApiPanelAttachmentControllerTest.php
@@ -1,5 +1,67 @@
 <?php
 
-test('view all panel-attachments', function () {
+use App\Models\Workstation;
+use App\Models\CarriagePanel;
+use App\Models\CarriageTrainset;
+use App\Support\Enums\IntentEnum;
+
+
+test('view all panelAttachment', function () {
+    createPanelAttachment();
     actAsSuperAdmin()->get('/api/panel-attachments')->assertStatus(200);
+});
+
+test('show one panelAttachment', function () {
+    $panelAttachment = createPanelAttachment();
+    actAsSuperAdmin()->get('/api/panel-attachments/' . $panelAttachment->id .'?intent=' . IntentEnum::API_PANEL_ATTACHMENT_GET_ATTACHMENT_DETAILS->value)->assertStatus(200);
+});
+
+test('show one panelAttachment with QR', function () {
+    $panelAttachment = createPanelAttachment();
+    actAsSuperAdmin()->get('/api/panel-attachments/' . $panelAttachment->id . '?intent=' . IntentEnum::API_PANEL_ATTACHMENT_GET_ATTACHMENT_DETAILS_WITH_QR->value . 'qr_code=' . $panelAttachment->qr_code)->assertStatus(200);
+});
+
+test('show one panelAttachment with invalid QR', function () {
+    $panelAttachment = createPanelAttachment();
+    actAsSuperAdmin()->get('/api/panel-attachments/' . $panelAttachment->id . '?intent=' . IntentEnum::API_PANEL_ATTACHMENT_GET_ATTACHMENT_DETAILS_WITH_QR->value . 'qr_code=' . $panelAttachment->qr_code . '123132')->assertStatus(400);
+});
+
+// test('show one panelAttachment with SN', function () {
+//     $panelAttachment = createPanelAttachment();
+//     actAsSuperAdmin()->get('/api/panel-attachments/' . $panelAttachment->id . '?intent=' . IntentEnum::IntentEnum::API_PANEL_ATTACHMENT_GET_ATTACHMENT_SERIAL_NUMBERS->value)->assertStatus(200);
+// });
+
+// THE REST IS PENDING
+
+test('store panelAttachment', function () {
+    createCarriagePanel();
+    createCarriageTrainset();
+    createWorkstation();
+    createWorkstation();
+    actAsSuperAdmin()->post('/api/panel-attachments', [
+        'carriage_trainset_id' => CarriageTrainset::inRandomOrder()->first()->id,
+        'carriage_panel_id' => CarriagePanel::inRandomOrder()->first()->id,
+        'source_workstation_id' => Workstation::inRandomOrder()->first()->id,
+        'destination_workstation_id' => Workstation::inRandomOrder()->first()->id,
+        'attachment_number' => 1,
+        'qr_code' => 'test qr',
+        'qr_path' => 'test qr',
+        'current_step' => 'test step',
+        'elapsed_time' => 1,
+        'status' => 'active',
+        'panel_attachment_id' => null,
+        'supervisor_id' => null,
+    ])->assertStatus(200);
+});
+
+test('update panelAttachment', function () {
+    $panelAttachment = createPanelAttachment();
+    actAsSuperAdmin()->put('/api/panel-attachments/' . $panelAttachment->id, [
+        'status' => 'active',
+    ])->assertStatus(200);
+});
+
+test('destroy panelAttachment', function () {
+    $panelAttachment = createPanelAttachment();
+    actAsSuperAdmin()->delete('/api/panel-attachments/' . $panelAttachment->id)->assertStatus(200);
 });
