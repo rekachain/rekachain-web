bbc3be670155adc803ab02af543b0064136111cc
N00budrag0n <helmiagung2468@gmail.com>
2024-09-24 15:27:36 +0700

    update: StepResouce.php adjust to model

--- app/Http/Resources/StepResource.php
+++ app/Http/Resources/StepResource.php
@@ -14,11 +14,8 @@ class StepResource extends JsonResource {
     public function toArray(Request $request): array {
         return [
             'id' => $this->id,
-            'progress_id' => $this->progress_id,
             'name' => $this->name,
             'process' => $this->process,
-            'progress' => ProgressResource::make($this->whenLoaded('progress')),
-            'user' => UserResource::make($this->whenLoaded('user')),
             'estimated_time' => $this->estimated_time,
             'can_be_deleted' => $this->canBeDeleted(),
         ];
