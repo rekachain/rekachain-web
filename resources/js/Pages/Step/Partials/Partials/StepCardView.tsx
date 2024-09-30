c0283acf299776aff2196f0729561c407a1bc015
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-09-30 13:28:54 +0700

    change typo route steps

--- resources/js/Pages/Step/Partials/Partials/StepCardView.tsx
+++ resources/js/Pages/Step/Partials/Partials/StepCardView.tsx
@@ -43,7 +43,7 @@ export default function StepCardView({
                         <div className="flex items-center justify-end w-full">
                             <Link
                                 className={buttonVariants({ variant: 'link' })}
-                                href={route(`${ROUTES.WORKSTATIONS}.edit`, step.id)}
+                                href={route(`${ROUTES.STEPS}.edit`, step.id)}
                             >
                                 Edit
                             </Link>
