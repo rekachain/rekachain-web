e7e99eb26d80dc175a817c8bec6bbda270ebe615
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-09-30 13:07:48 +0700

    add table view

--- resources/js/Pages/Step/Partials/Steps.tsx
+++ resources/js/Pages/Step/Partials/Steps.tsx
@@ -1,4 +1,3 @@
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/Components/UI/table';
 import { Link } from '@inertiajs/react';
 import { useEffect, useState } from 'react';
 import { StepResource } from '../../../Support/Interfaces/Resources';
@@ -12,6 +11,7 @@ import { stepService } from '@/Services/stepService';
 import { useSuccessToast } from '@/Hooks/useToast';
 import { useLoading } from '@/Contexts/LoadingContext';
 import StepCardView from './Partials/StepCardView';
+import StepTableView from './Partials/StepTableView';
 
 export default function () {
     const [stepResponse, setStepResponse] = useState<PaginateResponse<StepResource>>();
@@ -54,38 +54,10 @@ export default function () {
             {stepResponse && (
                 <>
                     <div className="hidden md:block">
-                        <Table>
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Nama</TableHead>
-                                    <TableHead>Proses</TableHead>
-                                    <TableHead>Estimasi Manufaktur</TableHead>
-                                    <TableHead></TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {stepResponse?.data.map(step => (
-                                    <TableRow key={step.id}>
-                                        <TableCell>{step.name}</TableCell>
-                                        <TableCell>{step.process}</TableCell>
-                                        <TableCell>{step.estimated_time}</TableCell>
-                                        <TableCell>
-                                            <Link
-                                                className={buttonVariants({ variant: 'link' })}
-                                                href={route(`${ROUTES.STEPS}.edit`, step.id)}
-                                            >
-                                                Edit
-                                            </Link>
-                                            {step.can_be_deleted && (
-                                                <Button variant="link" onClick={() => handleStepDeletion(step.id)}>
-                                                    Delete
-                                                </Button>
-                                            )}
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
+                        <StepTableView
+                            stepResponse={stepResponse}
+                            handleStepDeletion={handleStepDeletion}
+                        ></StepTableView>
                         {/* <WorkstationTableView
                     workstationResponse={workstationResponse}
                     handleWorkstationDeletion={handleWorkstationDeletion}
