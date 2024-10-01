3190ad518779b916493971f4085636c36ab38791
I <91296724+i9191@users.noreply.github.com>
2024-10-01 13:14:19 +0700

    draft detail worker on trainset attachment for worker and qc role

--- database/seeders/DetailWorkerTrainsetSeeder.php
+++ database/seeders/DetailWorkerTrainsetSeeder.php
@@ -21,7 +21,7 @@ class DetailWorkerTrainsetSeeder extends Seeder
         foreach ($trainsetAttachments as $trainsetAttachment) {
             $randomCount = rand(1, 5);
             $mekanikUsers = User::role(RoleEnum::WORKER_MEKANIK->value)->inRandomOrder()->take($randomCount)->get();
-            foreach ($mekanikUsers as $mekanikUser) {
+            foreach ($mekanikUsers as $user) {
                 // $progress_ids = [];
                 // $trainsets = $trainsetAttachment->trainset->carriage_trainsets()->get();
                 // foreach ($trainsets as $trainset) {
@@ -31,11 +31,11 @@ class DetailWorkerTrainsetSeeder extends Seeder
                 //     }
                 // }
                 // $progressSteps = ProgressStep::whereIn('progress_id', array_unique($progress_ids))
-                //     ->whereStepId($mekanikUser->step->id)->get();
+                //     ->whereStepId($elektrikUser->step->id)->get();
                 DetailWorkerTrainset::factory()->create([
                     'trainset_attachment_id' => $trainsetAttachment->id,
-                    'worker_id' => $mekanikUser->id,
-                    'progress_step_id' => ProgressStep::whereStepId($mekanikUser->step->id)->inRandomOrder()->first()->id,
+                    'worker_id' => $user->id,
+                    'progress_step_id' => ProgressStep::whereStepId($user->step->id)->inRandomOrder()->first()->id,
                 ]);
             }
             // DetailWorkerTrainset::factory()->create([
@@ -44,6 +44,27 @@ class DetailWorkerTrainsetSeeder extends Seeder
             //     'progress_step_id' => 1,
             // ]);
         }
+        $roles = [
+            // RoleEnum::WORKER_MEKANIK->value,
+            RoleEnum::WORKER_ELEKTRIK->value,
+            RoleEnum::QC_MEKANIK->value,
+            RoleEnum::QC_ELEKTRIK->value,
+        ];
+
+        foreach ($trainsetAttachments as $trainsetAttachment) {
+            foreach ($roles as $role) {
+                $randomCount = rand(1, 5);
+                $users = User::role($role)->inRandomOrder()->take($randomCount)->get();
+                foreach ($users as $user) {
+                    DetailWorkerTrainset::factory()->create([
+                        'trainset_attachment_id' => $trainsetAttachment->id,
+                        'worker_id' => $user->id,
+                        'progress_step_id' => ProgressStep::whereStepId($user->step->id)->inRandomOrder()->first()->id,
+                    ]);
+                }
+            }
+        }
+        $query = "select * from detail_worker_trainsets join progress_steps on progress_steps.id = detail_worker_trainsets.progress_step_id join progress on progress.id = progress_steps.progress_id";
         // DetailWorkerTrainset::factory()->create([
         //     'trainset_attachment_id' => 1,
         //     'worker_id' => 7,
