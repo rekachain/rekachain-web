a4c29912e9eab23185ce88d2515cad5d45bbbff5
I <91296724+i9191@users.noreply.github.com>
2024-10-01 11:28:52 +0700

    draft seeder detail worker for trainset

--- database/seeders/DetailWorkerTrainsetSeeder.php
+++ database/seeders/DetailWorkerTrainsetSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\DetailWorkerTrainset;
+use App\Models\ProgressStep;
+use App\Models\TrainsetAttachment;
+use App\Models\User;
+use App\Support\Enums\RoleEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,25 +17,52 @@ class DetailWorkerTrainsetSeeder extends Seeder
      */
     public function run(): void
     {
-        DetailWorkerTrainset::factory()->create([
-            'trainset_attachment_id' => 1,
-            'worker_id' => 7,
-            'progress_step_id' => 1,
-        ]);
-        DetailWorkerTrainset::factory()->create([
-            'trainset_attachment_id' => 1,
-            'worker_id' => 10,
-            'progress_step_id' => 3,
-        ]);
-        DetailWorkerTrainset::factory()->create([
-            'trainset_attachment_id' => 1,
-            'worker_id' => 8,
-            'progress_step_id' => 4,
-        ]);
-        DetailWorkerTrainset::factory()->create([
-            'trainset_attachment_id' => 1,
-            'worker_id' => 11,
-            'progress_step_id' => 7,
-        ]);
+        $trainsetAttachments = TrainsetAttachment::all();
+        foreach ($trainsetAttachments as $trainsetAttachment) {
+            $randomCount = rand(1, 5);
+            $mekanikUsers = User::role(RoleEnum::WORKER_MEKANIK->value)->inRandomOrder()->take($randomCount)->get();
+            foreach ($mekanikUsers as $mekanikUser) {
+                // $progress_ids = [];
+                // $trainsets = $trainsetAttachment->trainset->carriage_trainsets()->get();
+                // foreach ($trainsets as $trainset) {
+                //     $panels = $trainset->carriage_panels()->get();
+                //     foreach ($panels as $panel) {
+                //         $progress_ids = array_merge($progress_ids, [$panel->progress_id]);
+                //     }
+                // }
+                // $progressSteps = ProgressStep::whereIn('progress_id', array_unique($progress_ids))
+                //     ->whereStepId($mekanikUser->step->id)->get();
+                DetailWorkerTrainset::factory()->create([
+                    'trainset_attachment_id' => $trainsetAttachment->id,
+                    'worker_id' => $mekanikUser->id,
+                    'progress_step_id' => ProgressStep::whereStepId($mekanikUser->step->id)->inRandomOrder()->first()->id,
+                ]);
+            }
+            // DetailWorkerTrainset::factory()->create([
+            //     'trainset_attachment_id' => $trainsetAttachment->id,
+            //     'worker_id' => 1,
+            //     'progress_step_id' => 1,
+            // ]);
+        }
+        // DetailWorkerTrainset::factory()->create([
+        //     'trainset_attachment_id' => 1,
+        //     'worker_id' => 7,
+        //     'progress_step_id' => 1,
+        // ]);
+        // DetailWorkerTrainset::factory()->create([
+        //     'trainset_attachment_id' => 1,
+        //     'worker_id' => 10,
+        //     'progress_step_id' => 3,
+        // ]);
+        // DetailWorkerTrainset::factory()->create([
+        //     'trainset_attachment_id' => 1,
+        //     'worker_id' => 8,
+        //     'progress_step_id' => 4,
+        // ]);
+        // DetailWorkerTrainset::factory()->create([
+        //     'trainset_attachment_id' => 1,
+        //     'worker_id' => 11,
+        //     'progress_step_id' => 7,
+        // ]);
     }
 }
