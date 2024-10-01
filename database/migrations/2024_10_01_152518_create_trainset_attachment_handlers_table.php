91c64402c318a8b04e6a57d620fa6c9e54166c5b
ChamzalIsal <hhamzalnur10@gmail.com>
2024-10-02 02:04:13 +0700

    feature: seeder model trainset attachment handler

--- database/migrations/2024_10_01_152518_create_trainset_attachment_handlers_table.php
+++ database/migrations/2024_10_01_152518_create_trainset_attachment_handlers_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained();
             $table->foreignId('trainset_attachment_id')->constrained();
+            $table->enum('handles', ['prepare', 'send', 'receive']);
             $table->timestamps();
         });
     }
