5cff02771444d11cfb19efe58a1b67d95b7da1e7
I <91296724+i9191@users.noreply.github.com>
2024-10-03 10:08:47 +0700

    refactor detail worker trainset resource

--- app/Http/Resources/DetailWorkerTrainsetResource.php
+++ app/Http/Resources/DetailWorkerTrainsetResource.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class DetailWorkerTrainsetResource extends JsonResource {
     public function toArray($request): array {
         return [
-            'id' => $this->id,
+            'worker' => $this->worker,
+            'estimated_time' => $this->estimated_time,
+            'work_status' => $this->work_status,
+            'progress_step_id' => $this->progress_step_id,
+            'progress_step' => ProgressStepResource::make($this->whenLoaded('progress_step')),
+            'acceptance_status' => $this->acceptance_status,
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
         ];
