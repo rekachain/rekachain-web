d9b2affd0863b240d6cc9555a092395696e94270
I <91296724+i9191@users.noreply.github.com>
2024-10-03 09:53:18 +0700

    add progress step default resource

--- app/Http/Resources/ProgressStepResource.php
+++ app/Http/Resources/ProgressStepResource.php
@@ -8,6 +8,10 @@ class ProgressStepResource extends JsonResource {
     public function toArray($request): array {
         return [
             'id' => $this->id,
+            'progress_id' => $this->progress_id,
+            'progress' => ProgressResource::make($this->whenLoaded('progress')),
+            'step_id' => $this->step_id,
+            'step' => StepResource::make($this->whenLoaded('step')),
             'created_at' => $this->created_at->toDateTimeString(),
             'updated_at' => $this->updated_at->toDateTimeString(),
         ];
