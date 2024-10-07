91e0c1019ab617c31b25cf0677847e1ffa41669f
I <91296724+i9191@users.noreply.github.com>
2024-10-07 11:06:17 +0700

    fix detail worker relation on trainset attachment component

--- app/Models/TrainsetAttachmentComponent.php
+++ app/Models/TrainsetAttachmentComponent.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TrainsetAttachmentComponent extends Model
 {
@@ -26,5 +27,10 @@ class TrainsetAttachmentComponent extends Model
     public function carriage_panel_component(): BelongsTo
     {
         return $this->belongsTo(CarriagePanelComponent::class);
+    }
+
+    public function detail_worker_trainsets(): HasMany
+    {
+        return $this->hasMany(DetailWorkerTrainset::class);
     }
 }
