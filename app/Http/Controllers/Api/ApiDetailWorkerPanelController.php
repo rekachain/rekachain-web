87b55a1f43593bb297c5452a975bcc4c3260c0bf
ChamzalIsal <hhamzalnur10@gmail.com>
2024-10-10 09:40:51 +0700

    refact: api (SPV) assign Request work

--- app/Http/Controllers/Api/ApiDetailWorkerPanelController.php
+++ app/Http/Controllers/Api/ApiDetailWorkerPanelController.php
@@ -151,6 +151,10 @@ class ApiDetailWorkerPanelController extends Controller {
                 if (!$request->user()->hasRole(RoleEnum::SUPERVISOR_ASSEMBLY)) {
                     abort(403, 'Unauthorized');
                 }
+
+                $request->merge([
+                    'intent' => IntentEnum::API_DETAIL_WORKER_PANEL_GET_PANEL_DETAILS->value
+                ]);
                 
                 $data = $this->detailWorkerPanelService->requestAssign($detailWorkerPanel->id, $request);
 
