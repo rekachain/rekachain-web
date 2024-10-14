8ca8b1a3c8c5642f2ec92d6cc24680dee703ab3c
I <91296724+i9191@users.noreply.github.com>
2024-10-14 13:44:01 +0700

    edit trainset request assign worker with component id required and existed on trainset attachment

--- app/Http/Requests/TrainsetAttachment/UpdateTrainsetAttachmentRequest.php
+++ app/Http/Requests/TrainsetAttachment/UpdateTrainsetAttachmentRequest.php
@@ -6,21 +6,30 @@ use App\Support\Enums\IntentEnum;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateTrainsetAttachmentRequest extends FormRequest {
+    public function authorize(): bool {
+        return true;
+    }
     public function rules(): array {
         $intent = $this->get('intent');
 
         switch ($intent) {
             case IntentEnum::API_TRAINSET_ATTACHMENT_ASSIGN_WORKER->value:
-                return [
-                    'worker_id' => [
-                        'integer',
-                        'exists:users,id',
-                    ],
+                $arr = [
                     'carriage_panel_component_id' => [
+                        'required',
                         'integer',
-                        'exists:trainset_attachment_components,carriage_panel_component_id',
+                        'exists:trainset_attachment_components,carriage_panel_component_id,trainset_attachment_id,' . $this->route('trainset_attachment')->id,
                     ],
                 ];
+                if ($this->get('worker_id')) {
+                    $arr = array_merge($arr, [
+                        'worker_id' => [
+                            'integer',
+                            'exists:users,id',
+                        ]
+                    ]);
+                }
+                return $arr;
         }
         return [
             // Add your validation rules here
