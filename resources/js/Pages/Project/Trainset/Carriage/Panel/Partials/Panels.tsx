ef4168621eae454c04915483907bfcf0cad77a69
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-10-14 09:10:18 +0700

    add carriage panel cardview

--- resources/js/Pages/Project/Trainset/Carriage/Panel/Partials/Panels.tsx
+++ resources/js/Pages/Project/Trainset/Carriage/Panel/Partials/Panels.tsx
@@ -6,6 +6,7 @@ import { carriagePanelService } from '@/Services/carriagePanelService';
 import { useSuccessToast } from '@/Hooks/useToast';
 import { TrainsetStatusEnum } from '@/Support/Enums/trainsetStatusEnum';
 import { withLoading } from '@/Utils/withLoading';
+import CarPanelCardView from './Partials/CarPanelCardView';
 
 export default function ({
     trainset,
@@ -24,58 +25,72 @@ export default function ({
 
     return (
         <div className="space-y-4">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Nama</TableHead>
-                        <TableHead>Jumlah</TableHead>
-                        <TableHead>Deskripsi</TableHead>
-                        <TableHead>Proses</TableHead>
-                        <TableHead></TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {carriageTrainset?.carriage_panels?.map(carriage_panel => (
-                        <TableRow key={carriage_panel.id}>
-                            <TableCell>{carriage_panel.panel.name}</TableCell>
-                            <TableCell>
-                                {trainset.status === TrainsetStatusEnum.PROGRESS ? (
-                                    <span>{carriage_panel.qty}</span>
-                                ) : (
-                                    <PanelQty handleSyncCarriage={handleSyncCarriage} carriage_panel={carriage_panel} />
-                                )}
-                            </TableCell>
-                            <TableCell>{carriage_panel.panel.description}</TableCell>
-                            <TableCell>{carriage_panel.progress?.name}</TableCell>
-                            <TableCell>
-                                {/*<Link*/}
-                                {/*    className={buttonVariants({ variant: 'link' })}*/}
-                                {/*    href={route(`${ROUTES.PROJECTS_TRAINSETS}.edit`, carriage_panel.id)}*/}
-                                {/*>*/}
-                                {/*    Edit*/}
-                                {/*</Link>*/}
-
-                                {trainset.status !== TrainsetStatusEnum.PROGRESS && (
-                                    <Button variant="link" onClick={() => handlePanelDeletion(carriage_panel.id)}>
-                                        Delete
-                                    </Button>
-                                )}
-
-                                {/*<Link*/}
-                                {/*    className={buttonVariants({ variant: 'link' })}*/}
-                                {/*    href={route(`${ROUTES.PROJECTS_TRAINSETS_CARRIAGES_PANELS}.index`, [*/}
-                                {/*        carriage_panel.project_id,*/}
-                                {/*        carriage_panel.id,*/}
-                                {/*        carriage_panel.id,*/}
-                                {/*    ])}*/}
-                                {/*>*/}
-                                {/*    Detail*/}
-                                {/*</Link>*/}
-                            </TableCell>
+            <div className="hidden md:block">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Nama</TableHead>
+                            <TableHead>Jumlah</TableHead>
+                            <TableHead>Deskripsi</TableHead>
+                            <TableHead>Proses</TableHead>
+                            <TableHead></TableHead>
                         </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
+                    </TableHeader>
+                    <TableBody>
+                        {carriageTrainset?.carriage_panels?.map(carriage_panel => (
+                            <TableRow key={carriage_panel.id}>
+                                <TableCell>{carriage_panel.panel.name}</TableCell>
+                                <TableCell>
+                                    {trainset.status === TrainsetStatusEnum.PROGRESS ? (
+                                        <span>{carriage_panel.qty}</span>
+                                    ) : (
+                                        <PanelQty
+                                            handleSyncCarriage={handleSyncCarriage}
+                                            carriage_panel={carriage_panel}
+                                        />
+                                    )}
+                                </TableCell>
+                                <TableCell>{carriage_panel.panel.description}</TableCell>
+                                <TableCell>{carriage_panel.progress?.name}</TableCell>
+                                <TableCell>
+                                    {/*<Link*/}
+                                    {/*    className={buttonVariants({ variant: 'link' })}*/}
+                                    {/*    href={route(`${ROUTES.PROJECTS_TRAINSETS}.edit`, carriage_panel.id)}*/}
+                                    {/*>*/}
+                                    {/*    Edit*/}
+                                    {/*</Link>*/}
+
+                                    {trainset.status !== TrainsetStatusEnum.PROGRESS && (
+                                        <Button variant="link" onClick={() => handlePanelDeletion(carriage_panel.id)}>
+                                            Delete
+                                        </Button>
+                                    )}
+
+                                    {/*<Link*/}
+                                    {/*    className={buttonVariants({ variant: 'link' })}*/}
+                                    {/*    href={route(`${ROUTES.PROJECTS_TRAINSETS_CARRIAGES_PANELS}.index`, [*/}
+                                    {/*        carriage_panel.project_id,*/}
+                                    {/*        carriage_panel.id,*/}
+                                    {/*        carriage_panel.id,*/}
+                                    {/*    ])}*/}
+                                    {/*>*/}
+                                    {/*    Detail*/}
+                                    {/*</Link>*/}
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+
+            <div className="block md:hidden">
+                <CarPanelCardView
+                    trainset={trainset}
+                    carriageTrainset={carriageTrainset}
+                    handleSyncCarriage={handleSyncCarriage}
+                    handlePanelDeletion={handlePanelDeletion}
+                ></CarPanelCardView>
+            </div>
         </div>
     );
 }
