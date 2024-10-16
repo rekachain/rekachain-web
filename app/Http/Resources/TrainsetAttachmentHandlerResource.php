f0395e63c1e6ec068ddede3ef1c67eeb2ea9b9bf
I <91296724+i9191@users.noreply.github.com>
2024-10-16 10:19:19 +0700

    refact unfactored pending attachment and fix trainset assign spv

--- app/Http/Resources/TrainsetAttachmentHandlerResource.php
+++ app/Http/Resources/TrainsetAttachmentHandlerResource.php
@@ -2,16 +2,17 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TrainsetAttachmentHandlerResource extends JsonResource {
-    public function toArray($request): array {
+    public function toArray(Request $request): array {
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
             'user' => UserResource::make($this->whenLoaded('user')),
             'handler_name' => $this->handler_name,
-            'panel_attachment_id' => $this->panel_attachment_id,
+            'trainset_attachment_id' => $this->trainset_attachment_id,
             'handles' => $this->handles,
         ];
     }
