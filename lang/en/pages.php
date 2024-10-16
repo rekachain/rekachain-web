1bd33b481721a07ae7f8fff6b7eea70efb1cbbcd
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-10-16 13:24:21 +0700

    update: implement sidebar and page localization

---
+++ lang/en/pages.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+    'dashboard' => [
+        'index' => [
+            'title' => 'Dashboard',
+            'welcome' => 'Welcome to the Dashboard',
+        ],
+    ],
+    'work_days' => [
+        'index' => [
+            'title' => 'Work Days',
+            'buttons' => [
+                'create' => 'Create Work Day',
+            ],
+        ],
+        'create' => [
+            'title' => 'Create Work Day',
+        ],
+        'edit' => [
+            'title' => 'Edit Work Day',
+        ],
+    ],
+    'staff_management' => [
+        'title' => 'Staff Management',
+    ],
+    'divisions' => [
+        'title' => 'Divisions',
+    ],
+    'workshops' => [
+        'title' => 'Workshops',
+    ],
+    'workstations' => [
+        'title' => 'Workstations',
+    ],
+    'staff' => [
+        'title' => 'Staff',
+    ],
+    'access_control' => [
+        'title' => 'Access Control',
+    ],
+    'permissions' => [
+        'title' => 'Permissions',
+    ],
+    'roles' => [
+        'title' => 'Roles',
+    ],
+    'steps' => [
+        'title' => 'List Steps',
+    ],
+    'raw_materials' => [
+        'title' => 'List Raw Materials',
+    ],
+    'components' => [
+        'title' => 'List Components',
+    ],
+    'panels' => [
+        'title' => 'List Panels',
+    ],
+    'projects' => [
+        'title' => 'List Projects',
+    ],
+    'carriages' => [
+        'title' => 'List Carriages',
+    ],
+    'settings' => [
+        'title' => 'Settings',
+    ],
+    'feedback' => [
+        'title' => 'Feedback',
+    ],
+    'helpdesk' => [
+        'title' => 'Helpdesk',
+    ],
+];
