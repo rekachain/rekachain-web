8a30376bbbcf6da9645cdfb10fa428d85dfd36fa
I <91296724+i9191@users.noreply.github.com>
2024-10-17 14:21:30 +0700

    fix detail worker trainset resource and index api

--- app/Http/Controllers/Api/ApiDetailWorkerTrainsetController.php
+++ app/Http/Controllers/Api/ApiDetailWorkerTrainsetController.php
@@ -72,6 +72,8 @@ class ApiDetailWorkerTrainsetController extends Controller {
                         'work_status' => $status
                     ]
                 ]), $perPage));
+            default:
+                return DetailWorkerTrainsetResource::collection($this->detailWorkerTrainsetService->getAllPaginated($request->query(), $perPage));
         }
     }
 
@@ -92,14 +94,9 @@ class ApiDetailWorkerTrainsetController extends Controller {
      * Display the specified resource.
      */
     public function show(DetailWorkerTrainset $detailWorkerTrainset, Request $request) {
-        $request->merge(['intent' => IntentEnum::API_DETAIL_WORKER_TRAINSET_GET_TRAINSET_DETAILS->value]);
+        $request->merge(['intent' => IntentEnum::API_DETAIL_WORKER_TRAINSET_GET_WORK_DETAILS->value]);
 
-        return DetailWorkerTrainsetResource::collection($this->detailWorkerTrainsetService->getAllPaginated(array_merge($request->query(), [
-            'column_filters' => [
-                'worker_id'=> $request->user()->id,
-                'id' => $detailWorkerTrainset->id
-            ]
-        ])));
+        return DetailWorkerTrainsetResource::make($detailWorkerTrainset->load(['progress_step.progress','progress_step.step']));
     }
 
     /**
