ce82ec42034345a0cd1fda1a851c10c2511c1f4c
I <91296724+i9191@users.noreply.github.com>
2024-10-17 19:39:35 +0700

    refactor trainset attachment service and return if assign worker exist

--- app/Services/TrainsetAttachmentService.php
+++ app/Services/TrainsetAttachmentService.php
@@ -3,29 +3,43 @@
 namespace App\Services;
 
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\BaseCrudService;
-use App\Models\DetailWorkerTrainset;
-use App\Models\ProgressStep;
 use App\Models\TrainsetAttachment;
-use App\Models\TrainsetAttachmentComponent;
-use App\Models\User;
 use App\Support\Enums\TrainsetAttachmentHandlerHandlesEnum;
+use App\Support\Interfaces\Repositories\DetailWorkerTrainsetRepositoryInterface;
+use App\Support\Interfaces\Repositories\ProgressStepRepositoryInterface;
+use App\Support\Interfaces\Repositories\TrainsetAttachmentComponentRepositoryInterface;
 use App\Support\Interfaces\Repositories\TrainsetAttachmentRepositoryInterface;
+use App\Support\Interfaces\Repositories\UserRepositoryInterface;
+use App\Support\Interfaces\Services\DetailWorkerTrainsetServiceInterface;
 use App\Support\Interfaces\Services\TrainsetAttachmentServiceInterface;
 
 class TrainsetAttachmentService extends BaseCrudService implements TrainsetAttachmentServiceInterface {
+    public function __construct(
+        protected DetailWorkerTrainsetRepositoryInterface $detailWorkerTrainsetRepository,
+        protected ProgressStepRepositoryInterface $progressStepRepository,
+        protected TrainsetAttachmentComponentRepositoryInterface $trainsetAttachmentComponentRepository,
+        protected UserRepositoryInterface $userRepositoryInterface,
+        protected DetailWorkerTrainsetServiceInterface $detailWorkerTrainsetService,
+    ) {
+        parent::__construct();
+    }
     protected function getRepositoryClass(): string {
         return TrainsetAttachmentRepositoryInterface::class;
     }
 
     public function assignWorker(TrainsetAttachment $trainsetAttachment, array $data) {
         $userId = $data['worker_id'] ?? auth()->user()->id;
-        $user = User::find($userId);
-        $trainsetAttachmentComponent = TrainsetAttachmentComponent::find(['carriage_panel_component_id' => $data['carriage_panel_component_id'], 'trainset_attachment_id' => $trainsetAttachment->id])->first();
+        $user = $this->userRepositoryInterface->find($userId);
+        $trainsetAttachmentComponent = $this->trainsetAttachmentComponentRepository->findFirst(['carriage_panel_component_id' => $data['carriage_panel_component_id'], 'trainset_attachment_id' => $trainsetAttachment->id]);
         if ($trainsetAttachmentComponent) {
-            return DetailWorkerTrainset::create([
+            $workerTrainset = $this->detailWorkerTrainsetRepository->findFirst(['trainset_attachment_component_id' => $trainsetAttachmentComponent->id, 'worker_id' => $user->id]);
+            if ($workerTrainset) {
+                return $workerTrainset;
+            }
+            return $this->detailWorkerTrainsetService->create([
                 'trainset_attachment_component_id' => $trainsetAttachmentComponent->id,
                 'worker_id' => $user->id,
-                'progress_step_id' => ProgressStep::whereProgressId($trainsetAttachmentComponent->carriage_panel_component->progress_id)->whereStepId($user->step->id)->first()->id,
+                'progress_step_id' => $this->progressStepRepository->findFirst(['progress_id' => $trainsetAttachmentComponent->carriage_panel_component->progress_id, 'step_id' => $user->step->id])->id,
                 'estimated_time' => $user->step->estimated_time
             ]);
         }
