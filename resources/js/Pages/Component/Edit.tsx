a447eef09443425a13c25c7e66685bad8e00f761
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-10-25 21:17:29 +0700

    update: implement GenericDataSelector for component management page

--- resources/js/Pages/Component/Edit.tsx
+++ resources/js/Pages/Component/Edit.tsx
@@ -1,26 +1,20 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head, router, useForm } from '@inertiajs/react';
 import { Input } from '@/Components/UI/input';
-import { FormEventHandler, useEffect, useState } from 'react';
+import { FormEventHandler, useCallback, useEffect, useState } from 'react';
 import InputLabel from '@/Components/InputLabel';
 import { Button } from '@/Components/UI/button';
 import { componentService } from '@/Services/componentService';
 import { ROUTES } from '@/Support/Constants/routes';
 import { useSuccessToast } from '@/Hooks/useToast';
 import { ComponentResource, ProgressResource } from '@/Support/Interfaces/Resources';
-import { SelectGroup } from '@/Components/UI/select';
-import { Label } from '@/Components/UI/label';
-import { Check, ChevronsUpDown, RefreshCcw } from 'lucide-react';
-import { STYLING } from '@/Support/Constants/styling';
 import { PaginateResponse } from '@/Support/Interfaces/Others';
 import { withLoading } from '@/Utils/withLoading';
 import { useDebounce } from '@uidotdev/usehooks';
 import { ServiceFilterOptions } from '@/Support/Interfaces/Others/ServiceFilterOptions';
 import { progressService } from '@/Services/progressService';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/Components/UI/command';
-import { Popover, PopoverContent, PopoverTrigger } from '@/Components/UI/popover';
-import cn from 'mxcn';
 import { useLaravelReactI18n } from 'laravel-react-i18n';
+import GenericDataSelector from '@/Components/GenericDataSelector';
 
 export default function ({ component }: { component: ComponentResource }) {
     const { t } = useLaravelReactI18n();
@@ -28,8 +22,9 @@ export default function ({ component }: { component: ComponentResource }) {
     const [value, setValue] = useState('');
     const [progressResponse, setProgressResponse] = useState<PaginateResponse<ProgressResource>>();
     const [searchProgress, setSearchProgress] = useState(component.progress?.name);
+    console.log(component);
     const { data, setData, processing } = useForm({
-        progress_id: component.progress_id,
+        progress_id: component.progress_id as number | null,
         name: component.name,
     });
 
@@ -40,6 +35,10 @@ export default function ({ component }: { component: ComponentResource }) {
         const res = await progressService.getAll(filters);
         setProgressResponse(res);
     });
+
+    const fetchProgress = useCallback(async (filters: { search: string }) => {
+        return await progressService.getAll(filters).then(response => response.data);
+    }, []);
 
     useEffect(() => {
         void handleSyncProgress();
@@ -71,114 +70,20 @@ export default function ({ component }: { component: ComponentResource }) {
 
                     <form onSubmit={submit} encType="multipart/form-data">
                         <div className="mt-4">
-                            {/* TODO: refactor using GenericDataSelector, BUG: existing progress wont show */}
-                            <SelectGroup className="space-y-2">
-                                <div className="flex flex-col bg-background-2 gap-4 p-4">
-                                    <Label htmlFor="progress">{t('pages.component.edit.fields.progress')}</Label>
-                                    <div className="flex gap-2">
-                                        <Popover open={open} onOpenChange={setOpen}>
-                                            <PopoverTrigger asChild>
-                                                <Button
-                                                    variant="outline"
-                                                    role="combobox"
-                                                    aria-expanded={open}
-                                                    className="w-full justify-between"
-                                                >
-                                                    {value
-                                                        ? progressResponse?.data.find(
-                                                              progress => progress.name === value,
-                                                          )?.name
-                                                        : t('pages.component.edit.fields.progress')}
-                                                    <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                                                </Button>
-                                            </PopoverTrigger>
-                                            <PopoverContent className="w-full p-0">
-                                                <Command>
-                                                    <CommandInput
-                                                        onValueChange={e => setSearchProgress(e)}
-                                                        placeholder={t(
-                                                            'pages.component.edit.fields.progress_placeholder',
-                                                        )}
-                                                    />
-                                                    <CommandList>
-                                                        <CommandEmpty>No results found</CommandEmpty>
-                                                        <CommandGroup>
-                                                            {progressResponse?.data.map(progress => (
-                                                                <CommandItem
-                                                                    key={progress.name}
-                                                                    value={progress.name}
-                                                                    onSelect={currentValue => {
-                                                                        setData('progress_id', +progress.id);
-                                                                        setValue(
-                                                                            currentValue === value ? '' : currentValue,
-                                                                        );
-                                                                        setOpen(false);
-                                                                    }}
-                                                                >
-                                                                    <Check
-                                                                        className={cn(
-                                                                            'mr-2 h-4 w-4',
-                                                                            value === progress.name
-                                                                                ? 'opacity-100'
-                                                                                : 'opacity-0',
-                                                                        )}
-                                                                    />
-                                                                    {progress.name}
-                                                                </CommandItem>
-                                                            ))}
-                                                        </CommandGroup>
-                                                    </CommandList>
-                                                </Command>
-                                            </PopoverContent>
-                                        </Popover>
-                                        <Button
-                                            type="button"
-                                            variant="ghost"
-                                            onClick={setSearchProgress.bind(null, '')}
-                                        >
-                                            <RefreshCcw size={STYLING.ICON.SIZE.SMALL} />
-                                        </Button>
-                                    </div>
-                                    {/* <div className="flex gap-4">
-                                        <Input
-                                            placeholder="Cari progress"
-                                            value={searchProgress}
-                                            onChange={e => setSearchProgress(e.target.value)}
-                                            disabled={loading}
-                                        />
-                                        <Button
-                                            type="button"
-                                            variant="ghost"
-                                            onClick={setSearchProgress.bind(null, '')}
-                                        >
-                                            <RefreshCcw size={STYLING.ICON.SIZE.SMALL} />
-                                        </Button>
-                                    </div>
-                                    <div className="flex gap-4">
-                                        <Select
-                                            key={data.progress_id} // Force re-render when progress_id changes
-                                            onValueChange={v => setData('progress_id', +v)}
-                                            value={data.progress_id?.toString()}
-                                            disabled={loading}
-                                        >
-                                            <SelectTrigger id="progress">
-                                                <SelectValue placeholder="progress" />
-                                            </SelectTrigger>
-                                            <SelectContent>
-                                                <SelectItem value="0" defaultChecked disabled>
-                                                    {loading ? 'Loading' : 'Pilih progress'}
-                                                </SelectItem>
-                                                {progressResponse?.data.map(progress => (
-                                                    <SelectItem key={progress.id} value={progress.id.toString()}>
-                                                        {progress.name}
-                                                        <br />
-                                                    </SelectItem>
-                                                ))}
-                                            </SelectContent>
-                                        </Select>
-                                    </div> */}
-                                </div>
-                            </SelectGroup>
+                            <InputLabel htmlFor="progress">{t('pages.component.create.fields.progress')}</InputLabel>
+                            <div className="mt-4">
+                                <GenericDataSelector
+                                    id="progress_id"
+                                    fetchData={fetchProgress}
+                                    setSelectedData={id => setData('progress_id', id)}
+                                    selectedDataId={data.progress_id ?? undefined}
+                                    placeholder={t('pages.component.create.fields.progress_placeholder')}
+                                    renderItem={(item: ProgressResource) => item.name}
+                                    initialSearch={component.progress?.name}
+                                    buttonClassName="mt-1"
+                                    nullable
+                                />
+                            </div>
                         </div>
 
                         <div className="mt-4">
