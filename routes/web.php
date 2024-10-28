657092dd0164c7bebf26384e892ad66c88f25d20
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-10-28 10:01:03 +0700

    fix route for ziggy

--- routes/web.php
+++ routes/web.php
@@ -56,7 +56,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     
-    Route::resource('dashboard', DashboardController::class)->only('index')->middleware(['verified']);
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('divisions', DivisionController::class);
     Route::resource('workshops', WorkshopController::class);
     Route::resource('workstations', WorkstationController::class);
