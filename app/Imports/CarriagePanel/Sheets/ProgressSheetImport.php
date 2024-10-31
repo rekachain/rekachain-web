9ca164eff88b9086284125aa6b729b6e4dd28b8f
I <91296724+i9191@users.noreply.github.com>
2024-10-31 16:18:36 +0700

    fix progress query on progress import steps matching

--- app/Imports/CarriagePanel/Sheets/ProgressSheetImport.php
+++ app/Imports/CarriagePanel/Sheets/ProgressSheetImport.php
@@ -19,7 +19,7 @@ class ProgressSheetImport implements ToCollection
 
         $steps = collect();
         foreach ($rows->skip(1) as $row) {
-            if ($row[$header->search('Nama Proses')] == null || $row[$header->search('Nama Proses')] == '') {
+            if (empty($row[$header->search('Nama Proses')])) {
                 return;
             }
             if ($row[$header->search('Deskripsi')] == null || $row[$header->search('Deskripsi')] == '') {
@@ -33,17 +33,20 @@ class ProgressSheetImport implements ToCollection
                 ]));
             }
         }
-
-        $progress = Progress::whereWorkAspectId(WorkAspect::whereDivisionId(3)->first()->id)->whereHas('progress_steps.step', function ($query) use ($steps) {
-            $query->whereIn('name', $steps->pluck('name')->toArray());
-        })->whereHas('progress_steps', function ($query) use ($steps) {
-            $query->groupBy('progress_id')->havingRaw('count(*) = ' . count($steps));
-        })->first();
+        
+        $workAspectId = WorkAspect::whereDivisionId(3)->first()->id;
+        $progress = Progress::whereWorkAspectId($workAspectId)
+            ->whereHas('progress_steps', function ($query) use ($steps) {
+                $query->whereIn('step_id', $steps->pluck('id')->toArray())
+                    ->groupBy('progress_id')
+                    ->havingRaw('COUNT(*) = ?', [count($steps)]);
+            })
+            ->first();
 
         if (!$progress) {
             $progress = Progress::create([
                 'name' => 'Fitting & Koneksi - ' . $this->carriagePanel->panel->name,
-                'work_aspect_id' => WorkAspect::whereDivisionId(3)->first()->id
+                'work_aspect_id' => $workAspectId,
             ]);
             $progress->progress_steps()->createMany($steps->map(fn ($step) => ['step_id' => $step->id])->toArray());
         }
@@ -51,26 +54,4 @@ class ProgressSheetImport implements ToCollection
         $this->carriagePanel->update(['progress_id' => $progress->id]);
     }
 
-
-    public function model(array $row) 
-    {
-        $progress = Progress::whereWorkAspectId(WorkAspect::whereDivisionId(3)->first()->id)->get();
-
-        if (count($progress) > 0) {
-            
-        } else {
-            $progress = Progress::firstOrCreate([
-                'name' => 'Fitting & Koneksi - ' . $this->carriagePanel->panel->name,
-                'work_aspect_id' => WorkAspect::whereDivisionId(3)->first()->id
-            ]);
-            $step = Step::firstOrCreate([
-                'name' => $row['nama_proses'],
-                'process' => $row['deskripsi'],
-            ]);
-            $progress->progress_steps()->create([
-                'step_id' => $step->id
-            ]);
-            $this->carriagePanel->update(['progress_id' => $progress->id]);
-        }
-    }
 }
