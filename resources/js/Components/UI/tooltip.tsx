af10d2196edfde867a7997996c754e52b5107ffb
Galur Arasy Lumintang <adamhenderson3x3@gmail.com>
2024-11-01 18:25:59 +0700

    update: add tabs component, fixed cmdk useSyncExternalStore issue, and rename Lib/utils to Lib/Utils

--- resources/js/Components/UI/tooltip.tsx
+++ resources/js/Components/UI/tooltip.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
-import { cn } from "@/Lib/utils"
+import { cn } from "@/Lib/Utils"
 
 const TooltipProvider = TooltipPrimitive.Provider
 
