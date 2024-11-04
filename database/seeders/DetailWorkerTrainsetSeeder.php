dd92e0d3cf51f9fc4195db6c35fd4773c90d6bd5
I <91296724+i9191@users.noreply.github.com>
2024-11-04 12:35:44 +0700

    fix seeder to match business flow (for test)

--- database/seeders/DetailWorkerTrainsetSeeder.php
+++ database/seeders/DetailWorkerTrainsetSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\DetailWorkerTrainset;
-use App\Models\ProgressStep;
 use App\Models\TrainsetAttachment;
 use App\Models\User;
+use App\Support\Enums\DetailWorkerTrainsetAcceptanceStatusEnum;
+use App\Support\Enums\DetailWorkerTrainsetWorkStatusEnum;
 use App\Support\Enums\RoleEnum;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -23,29 +23,40 @@ class DetailWorkerTrainsetSeeder extends Seeder
             RoleEnum::QC_MEKANIK->value,
             RoleEnum::QC_ELEKTRIK->value,
         ];
-        $trainsetAttachments = TrainsetAttachment::all();
+        $trainsetAttachments = TrainsetAttachment::get();
 
         foreach ($trainsetAttachments as $trainsetAttachment) {
-            $trainsetAttachmentComponents = $trainsetAttachment->trainset_attachment_components()->get();
+            $trainsetAttachmentComponentsCount = $trainsetAttachment->trainset_attachment_components()->count();
+            $trainsetAttachmentComponents = $trainsetAttachment->trainset_attachment_components()->limit(rand(1, $trainsetAttachmentComponentsCount))->get();
             if (count($trainsetAttachmentComponents) == 0) {
                 continue;
             }
-            foreach ($trainsetAttachmentComponents as $trainsetAttachmentComponent) {
-                foreach ($roles as $role) {
-                    $randomCount = rand(1, 5);
-                    $users = User::role($role)->inRandomOrder()->take($randomCount)->get();
-                    foreach ($users as $user) {
-                        // $progressStep = ProgressStep::whereIn('progress_id', array_unique($progress_ids))
-                        //     ->whereStepId($user->step->id)->first();
-                        $progressStep = ProgressStep::whereProgressId($trainsetAttachmentComponent->carriage_panel_component->progress_id)
-                            ->whereStepId($user->step->id)->first();
-                        if (!$progressStep) {
-                            continue;
+            foreach ($trainsetAttachmentComponents as $key => $trainsetAttachmentComponent) {
+                $workStatus = DetailWorkerTrainsetWorkStatusEnum::COMPLETED->value;
+                $acceptanceStatus = DetailWorkerTrainsetAcceptanceStatusEnum::ACCEPTED->value;
+                $progressStepsCount = $trainsetAttachmentComponent->carriage_panel_component->progress->progress_steps()->count();
+                $progressSteps = $trainsetAttachmentComponent->carriage_panel_component->progress->progress_steps()->limit(rand(1, $progressStepsCount))->get();
+                foreach ($progressSteps as $key => $progressStep) {
+                    if ($key == $progressSteps->count() - 1) {
+                        $workStatus = array_rand([
+                            DetailWorkerTrainsetWorkStatusEnum::IN_PROGRESS->value => DetailWorkerTrainsetWorkStatusEnum::IN_PROGRESS->value,
+                            DetailWorkerTrainsetWorkStatusEnum::COMPLETED->value => DetailWorkerTrainsetWorkStatusEnum::COMPLETED->value,
+                        ]);
+                        $acceptanceStatus = $workStatus == DetailWorkerTrainsetWorkStatusEnum::COMPLETED->value ? DetailWorkerTrainsetAcceptanceStatusEnum::ACCEPTED->value : null;
+                        if($workStatus == DetailWorkerTrainsetWorkStatusEnum::COMPLETED->value && $progressSteps->count() == $progressStepsCount) {
+                            $trainsetAttachmentComponent->update([
+                               'total_fulfilled' => $trainsetAttachmentComponent->total_required 
+                            ]);
                         }
-                        DetailWorkerTrainset::factory()->create([
-                            'trainset_attachment_component_id' => $trainsetAttachmentComponent->id,
+                    }
+                    $users = User::whereStepId($progressStep->step_id)->inRandomOrder()->take(rand(1, 3))->get();
+                    foreach ($users as $user) {
+                        $trainsetAttachmentComponent->detail_worker_trainsets()->create([
                             'worker_id' => $user->id,
                             'progress_step_id' => $progressStep->id,
+                            'estimated_time' => $progressStep->step->estimated_time,
+                            'work_status' => $workStatus,
+                            'acceptance_status' => $acceptanceStatus
                         ]);
                     }
                 }
