a88690a8b775b5e88aa96675277ca2bce2fb56ef
ChamzalIsal <93560711+AkanTahu@users.noreply.github.com>
2024-11-07 13:08:59 +0700

    feat: seeder TrainsetAttachmentHandler

--- database/seeders/TrainsetAttachmentHandlerSeeder.php
+++ database/seeders/TrainsetAttachmentHandlerSeeder.php
@@ -2,24 +2,51 @@
 
 namespace Database\Seeders;
 
-use App\Models\TrainsetAttachmentHandler;
-use Database\Seeders\Helpers\CsvReader;
 use Illuminate\Database\Seeder;
+use App\Models\TrainsetAttachment;
+use Database\Seeders\Helpers\CsvReader;
+use App\Models\TrainsetAttachmentHandler;
+use App\Support\Enums\TrainsetAttachmentTypeEnum;
+use Faker\Factory as Faker;
 
 class TrainsetAttachmentHandlerSeeder extends Seeder {
     /**
      * Run the database seeds.
      */
     public function run(): void {
-        $csvReader = new CsvReader('trainset_attachment_handler');
-        $csvData = $csvReader->getCsvData();
-
-        if ($csvData) {
-            foreach ($csvData as $data) {
-                TrainsetAttachmentHandler::create($data);
+        $faker = Faker::create();
+        
+        $datas = TrainsetAttachment::all();
+        
+        $handles = ['prepare', 'send', 'receive'];
+        
+        foreach ($datas as $data){
+            foreach ($handles as $handle){
+                if ($handle == 'receive'){
+                    if ($data->type == TrainsetAttachmentTypeEnum::MECHANIC){
+                        TrainsetAttachmentHandler::create([
+                            'user_id' => '4',
+                            'handler_name' => 'Supervisor Mekanik',
+                            'trainset_attachment_id' => $data->id,
+                            'handles' => $handle,
+                        ]);
+                    }else {
+                        TrainsetAttachmentHandler::create([
+                            'user_id' => '5',
+                            'handler_name' => 'Supervisor Elektrik',
+                            'trainset_attachment_id' => $data->id,
+                            'handles' => $handle,
+                        ]);    
+                    }
+                } else {
+                    TrainsetAttachmentHandler::create([
+                        'user_id' => '3',
+                        'handler_name' => 'PPC_Pengendalian',
+                        'trainset_attachment_id' => $data->id,
+                        'handles' => $handle,
+                    ]); 
+                } 
             }
-        } else {
-            TrainsetAttachmentHandler::factory(1)->create();
         }
     }
 }
