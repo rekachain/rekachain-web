537872934abdeb4b865e2b344ba35342d62b5a9f
I <91296724+i9191@users.noreply.github.com>
2024-11-13 11:12:35 +0700

    handle service get import custom attachment material data template

--- app/Services/CustomAttachmentMaterialService.php
+++ app/Services/CustomAttachmentMaterialService.php
@@ -3,11 +3,27 @@
 namespace App\Services;
 
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\BaseCrudService;
+use App\Exports\CustomAttachmentMaterial\CustomAttachmentMaterialsTemplateExport;
+use App\Models\PanelAttachment;
 use App\Support\Interfaces\Repositories\CustomAttachmentMaterialRepositoryInterface;
 use App\Support\Interfaces\Services\CustomAttachmentMaterialServiceInterface;
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class CustomAttachmentMaterialService extends BaseCrudService implements CustomAttachmentMaterialServiceInterface {
     protected function getRepositoryClass(): string {
         return CustomAttachmentMaterialRepositoryInterface::class;
+    }
+
+    public function getImportDataTemplate(Model $model): BinaryFileResponse{
+        if ($model instanceof PanelAttachment) {
+            $prefix = $model->carriage_panel->panel->name . 
+                '-' . $model->carriage_panel->carriage_trainset->carriage->type . 
+                '-' . $model->carriage_panel->carriage_trainset->trainset->name . 
+                '-' . $model->carriage_panel->carriage_trainset->trainset->project->name;
+        } else {
+            $prefix = $model->type . '-' . $model->trainset->name . '-' . $model->trainset->project->name;
+        }
+        return (new CustomAttachmentMaterialsTemplateExport($model))->download($prefix . '-Raw Material Addition.xlsx');
     }
 }
--- app/Support/Interfaces/Services/CustomAttachmentMaterialServiceInterface.php
+++ app/Support/Interfaces/Services/CustomAttachmentMaterialServiceInterface.php
@@ -3,5 +3,9 @@
 namespace App\Support\Interfaces\Services;
 
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\Contracts\BaseCrudServiceInterface;
+use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
-interface CustomAttachmentMaterialServiceInterface extends BaseCrudServiceInterface {}
+interface CustomAttachmentMaterialServiceInterface extends BaseCrudServiceInterface {
+    public function getImportDataTemplate(Model $model): BinaryFileResponse;
+}
