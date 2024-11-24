b56b780f7acccd7055a94addabefbc1249a4ad48
I <91296724+i9191@users.noreply.github.com>
2024-11-24 13:44:13 +0700

    change trainset attachment seed to skip null trainset

--- database/seeders/TrainsetAttachmentSeeder.php
+++ database/seeders/TrainsetAttachmentSeeder.php
@@ -19,18 +19,31 @@ class TrainsetAttachmentSeeder extends Seeder {
         \Illuminate\Support\Facades\Auth::login($user);
         $csvReader = new CsvReader('trainset_attachment');
         $csvData = $csvReader->getCsvData();
+        if ($csvData) {
+            foreach ($csvData as $row) {
+                $trainset = Trainset::whereId($row['trainset_id'])->first();
+                if (is_null($trainset)) continue;
+                $data = [
+                    'division' => $row['division'],
+                    $row['division'].'_source_workstation_id' => $row['source_workstation_id'],
+                    $row['division'].'_destination_workstation_id' => $row['destination_workstation_id'],
+                ];
+                $this->trainsetService->generateTrainsetAttachment($trainset, $data);
+            }
+            return;
+        }
         $trainsets = Trainset::limit(10)->get();
         foreach ($trainsets as $key => $trainset) {
             $data = [
-                'division' => $csvData[$key*2]['division'] ?? TrainsetAttachmentTypeEnum::MECHANIC->value,
-                'mechanic_source_workstation_id' => $csvData[$key*2]['source_workstation_id'] ?? $sourceWorkstationId = Workstation::inRandomOrder()->first()->id,
-                'mechanic_destination_workstation_id' => $csvData[$key*2]['destination_workstation_id'] ?? Workstation::whereNotIn('name', ['Gudang','Ws. Harmonika'])->whereNot('id', $sourceWorkstationId)->inRandomOrder()->value('id'),
+                'division' => TrainsetAttachmentTypeEnum::MECHANIC->value,
+                'mechanic_source_workstation_id' => $sourceWorkstationId = Workstation::inRandomOrder()->first()->id,
+                'mechanic_destination_workstation_id' => Workstation::whereNotIn('name', ['Gudang','Ws. Harmonika'])->whereNot('id', $sourceWorkstationId)->inRandomOrder()->value('id'),
             ];
             $this->trainsetService->generateTrainsetAttachment($trainset, $data);
             $data = [
-                'division' => $csvData[$key*2+1]['division'] ?? TrainsetAttachmentTypeEnum::ELECTRIC->value,
-                'electric_source_workstation_id' => $csvData[$key*2+1]['source_workstation_id'] ?? $sourceWorkstationId = Workstation::inRandomOrder()->first()->id,
-                'electric_destination_workstation_id' => $csvData[$key*2+1]['destination_workstation_id'] ?? Workstation::whereNotIn('name', ['Gudang','Ws. Harmonika'])->whereNot('id', $sourceWorkstationId)->inRandomOrder()->value('id'),
+                'division' => TrainsetAttachmentTypeEnum::ELECTRIC->value,
+                'electric_source_workstation_id' => $sourceWorkstationId = Workstation::inRandomOrder()->first()->id,
+                'electric_destination_workstation_id' => Workstation::whereNotIn('name', ['Gudang','Ws. Harmonika'])->whereNot('id', $sourceWorkstationId)->inRandomOrder()->value('id'),
             ];
             $this->trainsetService->generateTrainsetAttachment($trainset, $data);
         }
