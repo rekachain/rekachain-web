d2b19d9cf8679bd652e0af7e5f6684756526f814
I <91296724+i9191@users.noreply.github.com>
2024-11-27 09:52:01 +0700

    add panel/assembly progress with worker on trainset

--- app/Http/Resources/TrainsetResource.php
+++ app/Http/Resources/TrainsetResource.php
@@ -234,6 +234,76 @@ class TrainsetResource extends JsonResource {
                     ];
                 });
                 return $panelProgress->toArray();
+            case IntentEnum::WEB_TRAINSET_GET_PANEL_PROGRESS_WITH_WORKER_STEPS->value:
+                $attachments = $this->panel_attachments->map(function (PanelAttachment $panelAttachment) {
+                    if ($panelAttachment->is_ancestor()) return $panelAttachment;
+                });
+                $panelProgress = $attachments->map(function (PanelAttachment $attachment) {
+                    $panelSteps = $attachment->progress->progress_steps->map(function ($progressStep) use (&$panelSteps) {
+                        return [
+                            'step_id' => $progressStep->step_id,
+                            'step_name' => $progressStep->step->name,
+                            'step_process' => $progressStep->step->process,
+                            'estimated_time' => $progressStep->step->estimated_time,
+                            'work_status' => null,
+                            'workers' => collect(),
+                        ];
+                    });
+
+                    $serialPanels =$attachment->serial_panels->map(function ($serialPanel) use ($panelSteps) {
+                        $steps = collect();
+                        $serialPanel->detail_worker_panels->map(function ($detailWorkerPanel) use (&$steps) {
+                            $workers = collect();
+                            $step = $steps->firstWhere('step_id', $detailWorkerPanel->progress_step->step_id);
+                            if (!$step) {
+                                $workers->push([
+                                    'nip' => $detailWorkerPanel->worker->nip,
+                                    'name' => $detailWorkerPanel->worker->name,
+                                    'started_at' => $detailWorkerPanel->created_at->toDateTimeString(),
+                                    'acceptance_status' => $detailWorkerPanel->acceptance_status,
+                                    'work_status' => $detailWorkerPanel->work_status
+                                ]);
+                                $steps->push([
+                                    'step_id' => $detailWorkerPanel->progress_step->step->id,
+                                    'step_name' => $detailWorkerPanel->progress_step->step->name,
+                                    'step_process' => $detailWorkerPanel->progress_step->step->process,
+                                    'estimated_time' => $detailWorkerPanel->progress_step->step->estimated_time,
+                                    'work_status' => $detailWorkerPanel->work_status->value,
+                                    'workers' => $workers
+                                ]);
+                            } else {
+                                $step['workers']->push([
+                                    'nip' => $detailWorkerPanel->worker->nip,
+                                    'name' => $detailWorkerPanel->worker->name,
+                                    'started_at' => $detailWorkerPanel->created_at->toDateTimeString(),
+                                    'acceptance_status' => $detailWorkerPanel->acceptance_status,
+                                    'work_status' => $detailWorkerPanel->work_status
+                                ]);
+                            }
+                        });
+                        logger($steps);
+                        $panelSteps->each(function ($panelStep) use (&$steps) {
+                            $step = $steps->firstWhere('step_id', $panelStep['step_id']);
+                            if (!$step) {
+                                $steps->push($panelStep);
+                            }
+                        });
+
+                        return [
+                            'serial_number' => $serialPanel->id,
+                            'product_no' => $serialPanel->product_no,
+                            'steps' => $steps->sortBy('step_id')->map(fn ($step) => $step)->values(),
+                        ];
+                    });
+
+                    return [
+                        'panel' => $attachment->carriage_panel->panel,
+                        'progress' => $attachment->carriage_panel->progress->fresh()->load('work_aspect'),
+                        'total_steps' => $panelSteps->count(),
+                        'serial_panels' => $serialPanels
+                    ];
+                });
+                return $panelProgress->toArray();
         }
 
         return [
