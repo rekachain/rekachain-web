133fff4685a56a0e369a0962cfb4ec576a756049
I <91296724+i9191@users.noreply.github.com>
2024-11-30 20:41:57 +0700

    implement enum localization

---
+++ app/Traits/Enums/Translatable.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Traits\Enums;
+
+trait Translatable {
+    public function getLabel(): string
+    {
+        $key = sprintf('%s.%s', static::class, $this->value);
+        return __('enums.'.$key);
+    }
+}
