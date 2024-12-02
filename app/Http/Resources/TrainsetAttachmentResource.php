59493518eab7f3e3ffd9d945aab2665327719fbd
I <91296724+i9191@users.noreply.github.com>
2024-12-02 15:57:14 +0700

    add carriage and panel on get trainset required component

--- app/Http/Resources/TrainsetAttachmentResource.php
+++ app/Http/Resources/TrainsetAttachmentResource.php
@@ -102,17 +102,26 @@ class TrainsetAttachmentResource extends JsonResource {
                     'components' => $components,
                 ];
             case IntentEnum::API_TRAINSET_ATTACHMENT_GET_ATTACHMENT_REQUIRED_COMPONENTS->value:
-                $trainsetAttachment = $this->ancestor()->load(['trainset_attachment_components']);
+                $trainsetAttachment = $this->ancestor();
                 $components = $trainsetAttachment->trainset_attachment_components->filter(function ($trainset_attachment_component) {
                     return $trainset_attachment_component->total_fulfilled !== $trainset_attachment_component->total_required;
                 })->map(function ($trainset_attachment_component) {
                     return [
                         'carriage_panel_component_id' => $trainset_attachment_component->carriage_panel_component_id,
+                        'carriage' => CarriageResource::make($trainset_attachment_component->carriage_panel_component->carriage_panel->carriage_trainset->carriage),
+                        'panel' => PanelResource::make($trainset_attachment_component->carriage_panel_component->carriage_panel->panel),
                         'component' => ComponentResource::make($trainset_attachment_component->carriage_panel_component->component),
                         'total_required' => $trainset_attachment_component->total_required,
                         'total_fulfilled' => $trainset_attachment_component->total_fulfilled,
                     ];
-                })->unique('component')->values();
+                });
+                if (isset($request->unique)) {
+                    if ($request->get('unique') == true) $components = $components->unique('component');
+                } else {
+                    $components = $components->unique('component');
+                }
+                $components = $components->values();
+
 
                 return [
                     'attachment_number' => $this->attachment_number,
