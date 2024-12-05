8646097775ccf637b2cc80360d77db431bcd6667
I <91296724+i9191@users.noreply.github.com>
2024-12-05 16:29:24 +0700

    handle custom related filterable search and column

--- app/Traits/Models/HasFilterable.php
+++ app/Traits/Models/HasFilterable.php
@@ -15,8 +15,12 @@ trait HasFilterable {
         $filterable['columns'] = $this->getColumnFilterableColumns();
 
         // Get filterable columns for related models
-        $filterable['relation_searchs'] = [];
-        $filterable['relation_columns'] = [];
+        $filterable['relation_searchs'] = $filterableRelatedSearchs = $this->getRelatedSearchFilterableColumns();
+        $filterable['relation_columns'] = $filterableRelatedColumns = $this->getRelatedColumnFilterableColumns();
+
+        if (!empty($filterableRelatedSearchs) && !empty($filterableRelatedColumns)) {
+            return $filterable;
+        }
 
         foreach ($this->getFilterableRelations() as $relationName) {
             $explodedRelation = explode('.', $relationName);
@@ -31,8 +35,8 @@ trait HasFilterable {
                 $relatedFilterable = $latestRelatedModel->getFilterable();
 
                 // Add related model's filterable columns to the current model's filterable columns
-                $filterable['relation_searchs'][$relationName] = $relatedFilterable['searchs'] ?? [];
-                $filterable['relation_columns'][$relationName] = $relatedFilterable['columns'] ?? [];
+                if (empty($filterableRelatedSearchs)) $filterable['relation_searchs'][$relationName] = $relatedFilterable['searchs'] ?? [];
+                if (empty($filterableRelatedColumns)) $filterable['relation_columns'][$relationName] = $relatedFilterable['columns'] ?? [];
             }
         }
 
@@ -41,13 +45,21 @@ trait HasFilterable {
 
     protected function getSearchFilterableColumns(): array {
         // Return the filterable search columns for the current model
-        // This can be a hardcoded array or a dynamic method
         return $this->filterable['searchs'] ?? [];
     }
 
     protected function getColumnFilterableColumns(): array {
         // Return the filterable columns for the current model
-        // This can be a hardcoded array or a dynamic method
         return $this->filterable['columns'] ?? [];
+    }
+
+    protected function getRelatedSearchFilterableColumns(): array {
+        // Return the related filterable search columns for the current model
+        return $this->filterable['relation_searchs'] ?? [];
+    }
+
+    protected function getRelatedColumnFilterableColumns(): array {
+        // Return the related filterable columns for the current model
+        return $this->filterable['relation_columns'] ?? [];
     }
 }
