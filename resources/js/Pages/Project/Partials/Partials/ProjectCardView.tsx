5e1f44e012f7658610c475745da2d8e8294193b8
Dewa <108676698+DewaMadeWira@users.noreply.github.com>
2024-12-08 20:50:16 +0700

    project button mobile view

--- resources/js/Pages/Project/Partials/Partials/ProjectCardView.tsx
+++ resources/js/Pages/Project/Partials/Partials/ProjectCardView.tsx
@@ -5,6 +5,9 @@ import { PaginateResponse } from '@/Support/Interfaces/Others';
 import { ProjectResource } from '@/Support/Interfaces/Resources';
 import { Link } from '@inertiajs/react';
 import { useLaravelReactI18n } from 'laravel-react-i18n';
+import { Select, SelectContent, SelectItem, SelectTrigger } from '@/Components/UI/select';
+import { Ellipsis } from 'lucide-react';
+import { STYLING } from '@/Support/Constants/styling';
 
 export default function ProjectCardView({
     projectResponse,
@@ -43,50 +46,61 @@ export default function ProjectCardView({
                         {/* <p>Jumlah User :{project.users_count}</p>
                         <p>Jumlah Izin :{project.permissions_count}</p> */}
                         <div className='flex w-full items-center justify-end'>
-                            <Link
-                                href={route(`${ROUTES.PROJECTS}.edit`, project.id)}
-                                className={buttonVariants({ variant: 'link' })}
-                                // className={buttonVariants({ variant: 'link' })}
-                                // href={route(`${ROUTES.DIVISIONS}.edit`, division.id)}
-                            >
-                                {t('action.edit')}
-                            </Link>
-                            <Button
-                                variant='link'
-                                onClick={() => handleProjectDeletion(project.id)}
-                            >
-                                {t('action.delete')}
-                            </Button>
-                            <Link
-                                href={route(`${ROUTES.PROJECTS_CARRIAGES}.index`, project.id)}
-                                className={buttonVariants({ variant: 'link' })}
-                            >
-                                {t(
-                                    'pages.project.partials.partials.project_table.actions.carriages',
-                                )}
-                            </Link>
-                            <Link
-                                href={route(`${ROUTES.PROJECTS_TRAINSETS}.index`, project.id)}
-                                className={buttonVariants({ variant: 'link' })}
-                            >
-                                {t(
-                                    'pages.project.partials.partials.project_card.actions.trainsets',
-                                )}
-                            </Link>
-                            <Link
-                                href={route(`${ROUTES.PROJECTS_COMPONENTS}.index`, project.id)}
-                                className={buttonVariants({ variant: 'link' })}
-                            >
-                                {t(
-                                    'pages.project.partials.partials.project_table.actions.components',
-                                )}
-                            </Link>
-                            <Link
-                                href={route(`${ROUTES.PROJECTS_PANELS}.index`, project.id)}
-                                className={buttonVariants({ variant: 'link' })}
-                            >
-                                {t('pages.project.partials.partials.project_table.actions.panels')}
-                            </Link>
+                <Select>
+                    <SelectTrigger className='w-fit border-none md:hidden'>
+                        <Ellipsis size={STYLING.ICON.SIZE.SMALL}></Ellipsis>
+                    </SelectTrigger>
+                    <SelectContent className=' '>
+                            <div className="flex flex-col gap-2 justify-start">
+                                <Link
+                                    href={route(`${ROUTES.PROJECTS}.edit`, project.id)}
+                                    className={buttonVariants({ variant: 'link' })}
+                                    // className={buttonVariants({ variant: 'link' })}
+                                    // href={route(`${ROUTES.DIVISIONS}.edit`, division.id)}
+                                >
+                                    {t('action.edit')}
+                                </Link>
+                                <Button
+                                    variant='link'
+                                    onClick={() => handleProjectDeletion(project.id)}
+                                >
+                                    {t('action.delete')}
+                                </Button>
+                                <Link
+                                    href={route(`${ROUTES.PROJECTS_CARRIAGES}.index`, project.id)}
+                                    className={buttonVariants({ variant: 'link' })}
+                                >
+                                    {t(
+                                        'pages.project.partials.partials.project_table.actions.carriages',
+                                    )}
+                                </Link>
+                                <Link
+                                    href={route(`${ROUTES.PROJECTS_TRAINSETS}.index`, project.id)}
+                                    className={buttonVariants({ variant: 'link' })}
+                                >
+                                    {t(
+                                        'pages.project.partials.partials.project_card.actions.trainsets',
+                                    )}
+                                </Link>
+                                <Link
+                                    href={route(`${ROUTES.PROJECTS_COMPONENTS}.index`, project.id)}
+                                    className={buttonVariants({ variant: 'link' })}
+                                >
+                                    {t(
+                                        'pages.project.partials.partials.project_table.actions.components',
+                                    )}
+                                </Link>
+                                <Link
+                                    href={route(`${ROUTES.PROJECTS_PANELS}.index`, project.id)}
+                                    className={buttonVariants({ variant: 'link' })}
+                                >
+                                    {t('pages.project.partials.partials.project_table.actions.panels')}
+                                </Link>
+                            </div>
+
+
+                    </SelectContent>
+                </Select>
                         </div>
                     </div>
                     {/* </div> */}
