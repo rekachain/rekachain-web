f14809a4687600c1117c7fa5fea111e21e0cb543
I <91296724+i9191@users.noreply.github.com>
2025-03-06 16:10:42 +0700

    add buyer fields for returned product form

--- resources/js/Pages/ReturnedProduct/Create.tsx
+++ resources/js/Pages/ReturnedProduct/Create.tsx
@@ -1,5 +1,6 @@
 import GenericDataSelector from '@/Components/GenericDataSelector';
 import InputLabel from '@/Components/InputLabel';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/Components/UI/accordion';
 import { Button } from '@/Components/UI/button';
 import { Input } from '@/Components/UI/input';
 import { Label } from '@/Components/UI/label';
@@ -17,7 +18,8 @@ import { ComponentResource, PanelResource } from '@/Support/Interfaces/Resources
 import { withLoading } from '@/Utils/withLoading';
 import { Head, router, useForm } from '@inertiajs/react';
 import { useLaravelReactI18n } from 'laravel-react-i18n';
-import { FormEventHandler, useCallback } from 'react';
+import { FormEventHandler, useCallback, useEffect } from 'react';
+import BuyerForm from './Partials/BuyerForm';
 
 export default function () {
     const { t } = useLaravelReactI18n();
@@ -65,6 +67,8 @@ export default function () {
         return await userService.getAll(filters).then((response) => response.data);
     }, []);
 
+    useEffect(() => setData('product_returnable_id', null), [data.product_returnable_type]);
+
     return (
         <>
             <Head title={t('pages.returned_product.create.title')} />
@@ -76,118 +80,127 @@ export default function () {
                         </h1>
                     </div>
 
-                    <form onSubmit={submit} encType='multipart/form-data'>
-                        <div className='mt-4 space-y-2 rounded bg-background-2 p-4'>
-                            <h2 className='text-lg font-semibold'>
-                                {t('pages.returned_product.create.fields.type')}
-                            </h2>
-                            <RadioGroup
-                                defaultValue={'component'}
-                                onValueChange={(v) => {
-                                    setData('product_returnable_type', v);
-                                    setData('product_returnable_id', null);
-                                }}
-                            >
-                                <div key={'component_type'} className='flex items-center space-x-2'>
-                                    <RadioGroupItem
-                                        value={'component'}
-                                        id={`type.component`}
-                                    />
-                                    <Label htmlFor={`type.component`}>
-                                        {'Komponen'}
-                                    </Label>
-                                </div>
-                                <div key={'panel_type'} className='flex items-center space-x-2'>
-                                    <RadioGroupItem
-                                        value={'panel'}
-                                        id={`type.panel`}
-                                    />
-                                    <Label htmlFor={`type.panel`}>
-                                        {'Panel'}
-                                    </Label>
-                                </div>
-                            </RadioGroup>
-                        </div>
-                        <div className='mt-4'>
-                            {data.product_returnable_type === 'component' ? (
-                                <>
-                                    <InputLabel
-                                        value={'Pilih Komponen'}
-                                        htmlFor='component_product_returnable_id'
-                                    />
-                                    <GenericDataSelector
-                                        setSelectedData={(id) => setData('product_returnable_id', id)}
-                                        selectedDataId={data.product_returnable_id ?? undefined}
-                                        renderItem={(item: ComponentResource) =>
-                                            `${item.name}`
-                                        } // Customize how to display the item
-                                        placeholder={'Pilih Komponen'}
-                                        nullable
-                                        id='component_product_returnable_id'
-                                        fetchData={fetchComponents}
-                                        buttonClassName='mt-1'
-                                        popoverContentClassName='w-[400px] p-0'
-                                    />
-                                </>
-                            ) : (
-                                <>
-                                    <InputLabel
-                                        value={'Pilih Panel'}
-                                        htmlFor='panel_product_returnable_id'
-                                    />
-                                    <GenericDataSelector
-                                        setSelectedData={(id) => setData('product_returnable_id', id)}
-                                        selectedDataId={data.product_returnable_id ?? undefined}
-                                        renderItem={(item: PanelResource) =>
-                                            `${item.name}`
-                                        } // Customize how to display the item
-                                        placeholder={'Pilih Panel'}
-                                        nullable
-                                        id='panel_product_returnable_id'
-                                        fetchData={fetchPanels}
-                                        buttonClassName='mt-1'
-                                        popoverContentClassName='w-[400px] p-0'
-                                    />
-                                </>
-                            )}
-                        </div>
+                    <form onSubmit={submit} id='returned-product-form' encType='multipart/form-data'> </form>
+                    <div className='mt-4 space-y-2 rounded bg-background-2 p-4'>
+                        <h2 className='text-lg font-semibold'>
+                            {t('pages.returned_product.create.fields.type')}
+                        </h2>
+                        <RadioGroup
+                            defaultValue={'component'}
+                            onValueChange={(v) => setData('product_returnable_type', v)}
+                        >
+                            <div key={'component_type'} className='flex items-center space-x-2'>
+                                <RadioGroupItem
+                                    value={'component'}
+                                    id={`type.component`}
+                                />
+                                <Label htmlFor={`type.component`}>
+                                    {'Komponen'}
+                                </Label>
+                            </div>
+                            <div key={'panel_type'} className='flex items-center space-x-2'>
+                                <RadioGroupItem
+                                    value={'panel'}
+                                    id={`type.panel`}
+                                />
+                                <Label htmlFor={`type.panel`}>
+                                    {'Panel'}
+                                </Label>
+                            </div>
+                        </RadioGroup>
+                    </div>
+                    <div className='mt-4'>
+                        {data.product_returnable_type === 'component' ? (
+                            <>
+                                <InputLabel
+                                    value={'Pilih Komponen'}
+                                    htmlFor='component_product_returnable_id'
+                                />
+                                <GenericDataSelector
+                                    setSelectedData={(id) => setData('product_returnable_id', id)}
+                                    selectedDataId={data.product_returnable_id ?? undefined}
+                                    renderItem={(item: ComponentResource) =>
+                                        `${item.name}`
+                                    } // Customize how to display the item
+                                    placeholder={'Pilih Komponen'}
+                                    nullable
+                                    id='component_product_returnable_id'
+                                    fetchData={fetchComponents}
+                                    buttonClassName='mt-1'
+                                    popoverContentClassName='w-[400px] p-0'
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <InputLabel
+                                    value={'Pilih Panel'}
+                                    htmlFor='panel_product_returnable_id'
+                                />
+                                <GenericDataSelector
+                                    setSelectedData={(id) => setData('product_returnable_id', id)}
+                                    selectedDataId={data.product_returnable_id ?? undefined}
+                                    renderItem={(item: PanelResource) =>
+                                        `${item.name}`
+                                    } // Customize how to display the item
+                                    placeholder={'Pilih Panel'}
+                                    nullable
+                                    id='panel_product_returnable_id'
+                                    fetchData={fetchPanels}
+                                    buttonClassName='mt-1'
+                                    popoverContentClassName='w-[400px] p-0'
+                                />
+                            </>
+                        )}
+                    </div>
 
-                        <div className='mt-4'>
-                            <InputLabel
-                                value={'Jumlah'}
-                                htmlFor='qty'
-                            />
-                            <Input
-                                value={data.qty}
-                                type='number'
-                                onChange={(e) => setData('qty', +e.target.value)}
-                                name='qty'
-                                id='qty'
-                                className='mt-1'
-                                autoComplete='qty'
-                            />
-                        </div>
+                    <div className='mt-4'>
+                        <InputLabel
+                            value={'Jumlah'}
+                            htmlFor='qty'
+                        />
+                        <Input
+                            value={data.qty}
+                            type='number'
+                            onChange={(e) => setData('qty', +e.target.value)}
+                            name='qty'
+                            id='qty'
+                            className='mt-1'
+                            autoComplete='qty'
+                        />
+                    </div>
 
-                        <div className='mt-4'>
-                            <InputLabel
-                                value={'Nomor Seri'}
-                                htmlFor='serial_number'
-                            />
-                            <Input
-                                value={data.serial_number ?? undefined}
-                                type='number'
-                                onChange={(e) => setData('serial_number', +e.target.value)}
-                                name='serial_number'
-                                id='serial_number'
-                                className='mt-1'
-                                autoComplete='serial_number'
-                            />
-                        </div>
+                    <div className='mt-4'>
+                        <InputLabel
+                            value={'Nomor Seri'}
+                            htmlFor='serial_number'
+                        />
+                        <Input
+                            value={data.serial_number ?? undefined}
+                            type='number'
+                            onChange={(e) => setData('serial_number', +e.target.value)}
+                            name='serial_number'
+                            id='serial_number'
+                            className='mt-1'
+                            autoComplete='serial_number'
+                        />
+                    </div>
 
-                        <Button disabled={loading} className='mt-4'>
-                            {t('pages.returned_product.create.buttons.submit')}
-                        </Button>
-                    </form>
+                    <Accordion type='single' collapsible className='mt-4'>
+                        <AccordionItem value='item-1'>
+                            <AccordionTrigger>
+                                {t('pages.project.create.fields.buyer_selection')}
+                            </AccordionTrigger>
+                            <AccordionContent>
+                                <BuyerForm
+                                    setBuyerId={(buyer_id: number) => setData('buyer_id', buyer_id)}
+                                />
+                            </AccordionContent>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <Button disabled={loading} form='returned-product-form' className='mt-4'>
+                        {t('pages.returned_product.create.buttons.submit')}
+                    </Button>
                 </div>
             </AuthenticatedLayout>
         </>
