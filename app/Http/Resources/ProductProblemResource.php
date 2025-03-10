83b1956f20f95fefb4ebb381b81d8fc60633e51a
I <91296724+i9191@users.noreply.github.com>
2025-03-10 12:09:34 +0700

    add product problem resources and controller with validation

---
+++ app/Http/Resources/ProductProblemResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductProblemResource extends JsonResource {
+    public function toArray($request): array {
+        return [
+            'id' => $this->id,
+            'returned_product_id' => $this->returned_product_id,
+            'returned_product' => ReturnedProductResource::make($this->whenLoaded('returned_product')),
+            'component_id' => $this->component_id,
+            'component' => ComponentResource::make($this->whenLoaded('component')),
+            'status' => $this->status,
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
