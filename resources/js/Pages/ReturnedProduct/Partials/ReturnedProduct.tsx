03f2f2c8ec76b007c556d7bd4ae8b7c91eae5dfd
I <91296724+i9191@users.noreply.github.com>
2025-03-12 11:42:58 +0700

    add ordering by creation date for returned products

--- resources/js/Pages/ReturnedProduct/Partials/ReturnedProduct.tsx
+++ resources/js/Pages/ReturnedProduct/Partials/ReturnedProduct.tsx
@@ -19,6 +19,7 @@ export default function () {
         page: 1,
         perPage: 10,
         relations: 'product_returnable,buyer',
+        orderBy: 'created_at',
     });
 
     const syncReturnedProducts = withLoading(async () => {
