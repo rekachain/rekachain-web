bfefe3347b2135d415654de3b2bb0b3241cdb84d
I <91296724+i9191@users.noreply.github.com>
2025-03-17 14:24:54 +0700

    add ReturnedProductNote migration, factory, and seeder

---
+++ database/migrations/2025_03_17_065107_create_returned_product_notes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('returned_product_notes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('returned_product_id')->constrained();
+            $table->text('note');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('returned_product_notes');
+    }
+};
