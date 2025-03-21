708dc26e0a71ffccf6b98481ae1232e680715955
I <91296724+i9191@users.noreply.github.com>
2025-03-21 17:00:23 +0700

    add ReplacementStock migration, factory, and seeder

---
+++ database/factories/ReplacementStockFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Component;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ReplacementStock>
+ */
+class ReplacementStockFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'component_id' => Component::inRandomOrder()->first()->id,
+            'threshold' => 0,
+            'qty' => $this->faker->numberBetween(5, 20),
+        ];
+    }
+}
---
+++ database/seeders/ReplacementStockSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\ReplacementStock;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ReplacementStockSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        ReplacementStock::factory(20)->create();
+    }
+}
