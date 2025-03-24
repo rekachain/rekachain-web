0fd719124e7212d4aa088978fd0acf5530d3cb6d
I <91296724+i9191@users.noreply.github.com>
2025-03-24 13:53:26 +0700

    add filterable trait and configuration to ReplacementStock model

--- app/Models/ReplacementStock.php
+++ app/Models/ReplacementStock.php
@@ -2,18 +2,31 @@
 
 namespace App\Models;
 
+use App\Traits\Models\HasFilterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ReplacementStock extends Model
 {
-    use HasFactory;
+    use HasFactory, HasFilterable;
 
     protected $fillable = [
         'component_id',
         'threshold',
         'qty',
+    ];
+    protected $filterable = [
+        'searchs' => [
+            'qty',
+        ],
+        'columns' => [],
+        'relation_searchs' => [
+            'component' => [
+                'name',
+                'description',
+            ],
+        ],
     ];
 
     public function component(): BelongsTo
