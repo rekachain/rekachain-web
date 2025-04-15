7722a12b3ac22b7f1e807f8150eeb6245f1deee0
I <91296724+i9191@users.noreply.github.com>
2025-04-15 17:11:31 +0700

    implement initiate Project service for product restock

--- app/Services/ProductRestockService.php
+++ app/Services/ProductRestockService.php
@@ -9,4 +9,57 @@ class ProductRestockService extends BaseCrudService implements ProductRestockSer
     protected function getRepositoryClass(): string {
         return ProductRestockRepositoryInterface::class;
     }
+
+    /**
+     * Initiate a restock project
+     *
+     * @param array $data = [
+     *     'project_name' => string,
+     *     'project_description' => string,
+     *     'project_initial_date' => string,
+     *     'panel_ids' => array,
+     *     'panel_qtys' => array
+     * ]
+     * @return bool
+     */
+    public function initiateRestockProject(array $data): bool {
+        $project = $this->projectService()->create([
+            'name' => $data['project_name'],
+            'description' => $data['project_description'],
+            'initial_date' => $data['project_initial_date'],
+            'trainset_needed' => 1,
+        ]);
+
+        $carriageTrainset = $this->carriageTrainsetService()->create([
+            'trainset_id' => $project->trainsets()->first()->id,
+        ]);
+
+        foreach ($data['panel_ids'] as $key => $panelId) {
+            $panel = $this->panelService()->findOrFail($panelId);
+            $panelQty = $data['panel_qtys'][$key];
+
+            $carriagePanel = $carriageTrainset->carriage_panels()->create([
+                'panel_id' => $panel->id,
+                'progress_id' => $panel->progress_id,
+                'qty' => $panelQty,
+            ]);
+
+            $carriagePanelComponents = $this->carriagePanelService()->find([
+                'panel_id' => $panel->id,
+            ])->first()->carriage_panel_components()->get();
+
+            if (count($carriagePanelComponents) > 0) {
+                foreach ($carriagePanelComponents as $carriagePanelComponent) {
+                    $carriagePanel->carriage_panel_components()->create([
+                        'component_id' => $carriagePanelComponent->component_id,
+                        'progress_id' => $carriagePanelComponent->progress_id,
+                        'qty' => $carriagePanelComponent->qty,
+                    ]);
+                }
+            }
+
+        }
+
+        return true;
+    }
 }
--- app/Support/Interfaces/Services/ProductRestockServiceInterface.php
+++ app/Support/Interfaces/Services/ProductRestockServiceInterface.php
@@ -4,4 +4,6 @@ namespace App\Support\Interfaces\Services;
 
 use Adobrovolsky97\LaravelRepositoryServicePattern\Services\Contracts\BaseCrudServiceInterface;
 
-interface ProductRestockServiceInterface extends BaseCrudServiceInterface {}
+interface ProductRestockServiceInterface extends BaseCrudServiceInterface {
+    public function initiateRestockProject(array $data): bool;
+}
