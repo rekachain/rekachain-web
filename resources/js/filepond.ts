8a41dc64bac1aa4789218921e923dbe2bcd0f8ee
I <91296724+i9191@users.noreply.github.com>
2025-04-27 19:23:00 +0700

    add filepond-plugin-file-validate-type for file type validation

--- resources/js/filepond.ts
+++ resources/js/filepond.ts
@@ -7,9 +7,11 @@ import 'filepond-plugin-file-poster/dist/filepond-plugin-file-poster.css';
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
+import FilepondPluginFileValidateType from 'filepond-plugin-file-validate-type';
 
 registerPlugin(
     FilePondPluginImageExifOrientation,
     FilePondPluginImagePreview,
     FilePondPluginFilePoster,
+    FilepondPluginFileValidateType,
 );
