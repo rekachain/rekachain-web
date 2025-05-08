7292b320b69c74fdf0411b766ccc88f5a77c2651
I <91296724+i9191@users.noreply.github.com>
2025-05-08 22:16:00 +0700

    feat: retreive localized array of enums

--- resources/js/Helpers/enumHelper.ts
+++ resources/js/Helpers/enumHelper.ts
@@ -1,11 +1,21 @@
-export async function fetchEnumLabels(enumName: string): Promise<Record<string, string>> {
-    const response = await fetch(`/api/enums/${enumName}`, {
-        headers: {
-            'Accept-Language': String(window.axios.defaults.headers['Accept-Language'] || ''),
-        },
-    });
+export async function fetchEnumLabels(enumNames: string | string[]): Promise<Record<string, string> | any> {
+    let response: Response;
+    if (Array.isArray(enumNames)) {
+        const params = enumNames.map((enumName) => `enums[]=${enumName}`).join('&');
+        response = await fetch(`/api/enums/all?${params}`, {
+            headers: {
+                'Accept-Language': String(window.axios.defaults.headers['Accept-Language'] || ''),
+            },
+        });
+    } else {
+        response = await fetch(`/api/enums/${enumNames}`, {
+            headers: {
+                'Accept-Language': String(window.axios.defaults.headers['Accept-Language'] || ''),
+            },
+        });
+    }
     if (!response.ok) {
-        throw new Error(`Failed to fetch labels for enum: ${enumName}`);
+        throw new Error(`Failed to fetch labels for enum: ${enumNames} (${response.status} ${response.statusText})`);
     }
     return response.json();
 }
