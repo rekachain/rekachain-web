a9ba8e1b4981eb9598b4cc528cc0975723443637
I <91296724+i9191@users.noreply.github.com>
2025-07-03 13:11:38 +0700

    Add deployment workflows for development and production path

--- deploy.php
+++ deploy.php
@@ -41,6 +41,11 @@ task('deploy:secrets', function () {
 host('prod')
     ->set('remote_user', 'rekachain')
     ->setHostname('103.211.26.90')
+    ->set('deploy_path', '/var/www/rekachain.rekaindo.prod');
+    
+host('main')
+    ->set('remote_user', 'rekachain')
+    ->setHostname('103.211.26.90')
     ->set('deploy_path', '/var/www/rekachain.dhanifudin.com');
 
 // Hooks
